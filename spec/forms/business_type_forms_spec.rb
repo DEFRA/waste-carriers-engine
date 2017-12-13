4cf463af822250cb97f08311c35bc4bfd483daba
irisfaraway <iris.faraway@gmail.com>
2017-12-13 14:34:05 +0000

    Endlessly chasing unit test coverage stats

--- spec/forms/business_type_forms_spec.rb
+++ spec/forms/business_type_forms_spec.rb
@@ -1,6 +1,26 @@
 require "rails_helper"
 
 RSpec.describe BusinessTypeForm, type: :model do
+  describe "#submit" do
+    context "when the form is valid" do
+      let(:business_type_form) { build(:business_type_form, :has_required_data) }
+      let(:valid_params) { { reg_identifier: business_type_form.reg_identifier } }
+
+      it "should submit" do
+        expect(business_type_form.submit(valid_params)).to eq(true)
+      end
+    end
+
+    context "when the form is valid" do
+      let(:business_type_form) { build(:business_type_form, :has_required_data) }
+      let(:invalid_params) { { reg_identifier: "foo" } }
+
+      it "should not submit" do
+        expect(business_type_form.submit(invalid_params)).to eq(false)
+      end
+    end
+  end
+
   describe "#reg_identifier" do
     context "when a valid transient registration exists" do
       let(:transient_registration) do
@@ -9,7 +29,7 @@ RSpec.describe BusinessTypeForm, type: :model do
                workflow_state: "business_type_form")
       end
       # Don't use FactoryBot for this as we need to make sure it initializes with a specific object
-      let(:business_type_form) { RenewalStartForm.new(transient_registration) }
+      let(:business_type_form) { BusinessTypeForm.new(transient_registration) }
 
       context "when a reg_identifier meets the requirements" do
         before(:each) do
@@ -31,14 +51,16 @@ RSpec.describe BusinessTypeForm, type: :model do
         end
       end
     end
+  end
 
+  describe "#transient_registration" do
     context "when the transient registration is invalid" do
       let(:transient_registration) do
         build(:transient_registration,
               workflow_state: "business_type_form")
       end
       # Don't use FactoryBot for this as we need to make sure it initializes with a specific object
-      let(:business_type_form) { RenewalStartForm.new(transient_registration) }
+      let(:business_type_form) { BusinessTypeForm.new(transient_registration) }
 
       it "is not valid" do
         business_type_form.reg_identifier = transient_registration.reg_identifier
