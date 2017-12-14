4301fad1504696efa9ee9005fa4c7f8474fa06c2
irisfaraway <iris.faraway@gmail.com>
2017-12-14 10:29:23 +0000

    DRY up RenwalStartFormsController, reorder methods

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -25,32 +25,6 @@ class FormsController < ApplicationController
 
   protected
 
-  def set_transient_registration(reg_identifier)
-    @transient_registration = TransientRegistration.where(reg_identifier: reg_identifier).first ||
-                              TransientRegistration.new(reg_identifier: reg_identifier)
-  end
-
-  def submit_form(form, params)
-    respond_to do |format|
-      if form.submit(params)
-        @transient_registration.next!
-        format.html { redirect_to_correct_form }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-
-  def form_matches_state?
-    controller_name == "#{@transient_registration.workflow_state}s"
-  end
-
-  def redirect_to_correct_form
-    redirect_to form_path
-  end
-
-  private
-
   # Expects a form class name (eg BusinessTypeForm), a snake_case name for the form (eg business_type_form),
   # and the reg_identifier param
   def set_up_form(form_class, form, reg_identifier)
@@ -65,9 +39,35 @@ class FormsController < ApplicationController
     instance_variable_set("@#{form}", form_class.new(@transient_registration))
   end
 
+  private
+
+  def set_transient_registration(reg_identifier)
+    @transient_registration = TransientRegistration.where(reg_identifier: reg_identifier).first ||
+                              TransientRegistration.new(reg_identifier: reg_identifier)
+  end
+
+  def form_matches_state?
+    controller_name == "#{@transient_registration.workflow_state}s"
+  end
+
+  def redirect_to_correct_form
+    redirect_to form_path
+  end
+
   # Get the path based on the workflow state, with reg_identifier as params, ie:
   # new_state_name_path/:reg_identifier
   def form_path
     send("new_#{@transient_registration.workflow_state}_path".to_sym, @transient_registration.reg_identifier)
+  end
+
+  def submit_form(form, params)
+    respond_to do |format|
+      if form.submit(params)
+        @transient_registration.next!
+        format.html { redirect_to_correct_form }
+      else
+        format.html { render :new }
+      end
+    end
   end
 end
--- app/controllers/renewal_start_forms_controller.rb
+++ app/controllers/renewal_start_forms_controller.rb
@@ -2,15 +2,8 @@ class RenewalStartFormsController < FormsController
 
   # Unlike other forms, we don't use 'super' for this action because we need to run different validations
   def new
-    set_transient_registration(params[:reg_identifier])
-
-    if !form_matches_state?
-      redirect_to_correct_form
-    else
-      @renewal_start_form = RenewalStartForm.new(@transient_registration)
-      # Run validations now so we know if the registration exists and can be renewed
-      @renewal_start_form.validate
-    end
+    return unless set_up_form(RenewalStartForm, "renewal_start_form", params[:reg_identifier])
+    @renewal_start_form.validate
   end
 
   def create
