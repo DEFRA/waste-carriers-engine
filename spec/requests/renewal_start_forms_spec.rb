f0c1cee8acae8c37be92a84de84f5110389d0e3b
irisfaraway <iris.faraway@gmail.com>
2017-12-19 13:42:27 +0000

    Add tests for different users

--- spec/requests/renewal_start_forms_spec.rb
+++ spec/requests/renewal_start_forms_spec.rb
@@ -3,20 +3,10 @@ require "rails_helper"
 RSpec.describe "RenewalStartForms", type: :request do
   describe "GET new_renewal_start_form_path" do
     context "when a user is signed in" do
+      let(:user) { create(:user) }
+
       before(:each) do
-        user = create(:user)
         sign_in(user)
-      end
-
-      context "when no renewal is in progress" do
-        context "when a matching registration exists" do
-          let(:registration) { create(:registration, :has_required_data, :expires_soon) }
-
-          it "returns a success response" do
-            get new_renewal_start_form_path(registration[:reg_identifier])
-            expect(response).to have_http_status(200)
-          end
-        end
       end
 
       context "when no matching registration exists" do
@@ -33,30 +23,75 @@ RSpec.describe "RenewalStartForms", type: :request do
         end
       end
 
-      context "when a renewal is in progress" do
-        context "when a valid transient registration exists" do
-          let(:transient_registration) do
-            create(:transient_registration,
-                   :has_required_data,
-                   workflow_state: "renewal_start_form")
+      context "when a matching registration exists" do
+        context "when the signed-in user owns the registration" do
+          context "when no renewal is in progress" do
+            let(:registration) { create(:registration, :has_required_data, :expires_soon, account_email: user.email) }
+
+            it "returns a success response" do
+              get new_renewal_start_form_path(registration[:reg_identifier])
+              expect(response).to have_http_status(200)
+            end
           end
 
-          it "returns a success response" do
-            get new_renewal_start_form_path(transient_registration[:reg_identifier])
-            expect(response).to have_http_status(200)
+          context "when a renewal is in progress" do
+            context "when a valid transient registration exists" do
+              let(:transient_registration) do
+                create(:transient_registration,
+                       :has_required_data,
+                       account_email: user.email,
+                       workflow_state: "renewal_start_form")
+              end
+
+              it "returns a success response" do
+                get new_renewal_start_form_path(transient_registration[:reg_identifier])
+                expect(response).to have_http_status(200)
+              end
+            end
+
+            context "when the transient registration is in a different state" do
+              let(:transient_registration) do
+                create(:transient_registration,
+                       :has_required_data,
+                       account_email: user.email,
+                       workflow_state: "business_type_form")
+              end
+
+              it "redirects to the form for the current state" do
+                get new_renewal_start_form_path(transient_registration[:reg_identifier])
+                expect(response).to redirect_to(new_business_type_form_path(transient_registration[:reg_identifier]))
+              end
+            end
           end
         end
 
-        context "when the transient registration is in a different state" do
-          let(:transient_registration) do
-            create(:transient_registration,
-                   :has_required_data,
-                   workflow_state: "business_type_form")
+        context "when the signed-in user does not own the registration" do
+          context "when no renewal is in progress" do
+            let(:registration) do
+              create(:registration,
+                     :has_required_data,
+                     :expires_soon,
+                     account_email: "not-#{user.email}")
+            end
+
+            it "prevents access" do
+              get new_renewal_start_form_path(registration[:reg_identifier])
+              skip("don't know what this should do yet")
+            end
           end
 
-          it "redirects to the form for the current state" do
-            get new_renewal_start_form_path(transient_registration[:reg_identifier])
-            expect(response).to redirect_to(new_business_type_form_path(transient_registration[:reg_identifier]))
+          context "when a renewal is in progress" do
+            let(:transient_registration) do
+              create(:transient_registration,
+                     :has_required_data,
+                     account_email: "not-#{user.email}",
+                     workflow_state: "renewal_start_form")
+            end
+
+            it "prevents access" do
+              get new_renewal_start_form_path(transient_registration[:reg_identifier])
+              skip("don't know what this should do yet")
+            end
           end
         end
       end
@@ -82,56 +117,10 @@ RSpec.describe "RenewalStartForms", type: :request do
 
   describe "POST renewal_start_forms_path" do
     context "when a user is signed in" do
+      let(:user) { create(:user) }
+
       before(:each) do
-        user = create(:user)
         sign_in(user)
-      end
-
-      context "when no renewal is in progress" do
-        context "when a matching registration exists" do
-          let(:registration) { create(:registration, :has_required_data, company_name: "Correct Name") }
-
-          context "when valid params are submitted" do
-            let(:valid_params) { { reg_identifier: registration.reg_identifier } }
-
-            it "creates a new transient registration" do
-              expected_tr_count = TransientRegistration.count + 1
-              post renewal_start_forms_path, renewal_start_form: valid_params
-              updated_tr_count = TransientRegistration.count
-
-              expect(expected_tr_count).to eq(updated_tr_count)
-            end
-
-            it "creates a transient registration with correct data" do
-              post renewal_start_forms_path, renewal_start_form: valid_params
-              transient_registration = TransientRegistration.where(reg_identifier: registration.reg_identifier).first
-
-              expect(transient_registration.reg_identifier).to eq(registration.reg_identifier)
-              expect(transient_registration.company_name).to eq(registration.company_name)
-            end
-
-            it "returns a 302 response" do
-              post renewal_start_forms_path, renewal_start_form: valid_params
-              expect(response).to have_http_status(302)
-            end
-
-            it "redirects to the business type form" do
-              post renewal_start_forms_path, renewal_start_form: valid_params
-              expect(response).to redirect_to(new_business_type_form_path(valid_params[:reg_identifier]))
-            end
-          end
-
-          context "when invalid params are submitted" do
-            let(:invalid_params) { { reg_identifier: "foo" } }
-
-            it "does not create a new transient registration" do
-              original_tr_count = TransientRegistration.count
-              post renewal_start_forms_path, renewal_start_form: invalid_params
-              updated_tr_count = TransientRegistration.count
-              expect(original_tr_count).to eq(updated_tr_count)
-            end
-          end
-        end
       end
 
       context "when no matching registration exists" do
@@ -168,41 +157,65 @@ RSpec.describe "RenewalStartForms", type: :request do
         end
       end
 
-      context "when a renewal is in progress" do
-        let(:transient_registration) do
-          create(:transient_registration,
-                 :has_required_data,
-                 workflow_state: "renewal_start_form")
+      context "when the signed-in user owns the registration" do
+        context "when a matching registration exists" do
+          context "when no renewal is in progress" do
+            let(:registration) do
+              create(:registration,
+                     :has_required_data,
+                     account_email: user.email,
+                     company_name: "Correct Name")
+            end
+
+            context "when valid params are submitted" do
+              let(:valid_params) { { reg_identifier: registration.reg_identifier } }
+
+              it "creates a new transient registration" do
+                expected_tr_count = TransientRegistration.count + 1
+                post renewal_start_forms_path, renewal_start_form: valid_params
+                updated_tr_count = TransientRegistration.count
+
+                expect(expected_tr_count).to eq(updated_tr_count)
+              end
+
+              it "creates a transient registration with correct data" do
+                post renewal_start_forms_path, renewal_start_form: valid_params
+                transient_registration = TransientRegistration.where(reg_identifier: registration.reg_identifier).first
+
+                expect(transient_registration.reg_identifier).to eq(registration.reg_identifier)
+                expect(transient_registration.company_name).to eq(registration.company_name)
+              end
+
+              it "returns a 302 response" do
+                post renewal_start_forms_path, renewal_start_form: valid_params
+                expect(response).to have_http_status(302)
+              end
+
+              it "redirects to the business type form" do
+                post renewal_start_forms_path, renewal_start_form: valid_params
+                expect(response).to redirect_to(new_business_type_form_path(valid_params[:reg_identifier]))
+              end
+            end
+
+            context "when invalid params are submitted" do
+              let(:invalid_params) { { reg_identifier: "foo" } }
+
+              it "does not create a new transient registration" do
+                original_tr_count = TransientRegistration.count
+                post renewal_start_forms_path, renewal_start_form: invalid_params
+                updated_tr_count = TransientRegistration.count
+                expect(original_tr_count).to eq(updated_tr_count)
+              end
+            end
+          end
         end
 
-        let(:valid_params) { { reg_identifier: transient_registration.reg_identifier } }
-
-        it "returns a 302 response" do
-          post renewal_start_forms_path, renewal_start_form: valid_params
-          expect(response).to have_http_status(302)
-        end
-
-        it "redirects to the business type form" do
-          post renewal_start_forms_path, renewal_start_form: valid_params
-          expect(response).to redirect_to(new_business_type_form_path(valid_params[:reg_identifier]))
-        end
-
-        it "does not create a new transient registration" do
-          # Touch the test object so it gets created now and the count is correct
-          transient_registration.touch
-
-          original_tr_count = TransientRegistration.count
-          post renewal_start_forms_path, renewal_start_form: valid_params
-          updated_tr_count = TransientRegistration.count
-
-          expect(original_tr_count).to eq(updated_tr_count)
-        end
-
-        context "when the state is different" do
+        context "when a renewal is in progress" do
           let(:transient_registration) do
             create(:transient_registration,
                    :has_required_data,
-                   workflow_state: "smart_answers_form")
+                   account_email: user.email,
+                   workflow_state: "renewal_start_form")
           end
 
           let(:valid_params) { { reg_identifier: transient_registration.reg_identifier } }
@@ -212,9 +225,9 @@ RSpec.describe "RenewalStartForms", type: :request do
             expect(response).to have_http_status(302)
           end
 
-          it "redirects to the correct form" do
+          it "redirects to the business type form" do
             post renewal_start_forms_path, renewal_start_form: valid_params
-            expect(response).to redirect_to(new_smart_answers_form_path(valid_params[:reg_identifier]))
+            expect(response).to redirect_to(new_business_type_form_path(valid_params[:reg_identifier]))
           end
 
           it "does not create a new transient registration" do
@@ -226,6 +239,70 @@ RSpec.describe "RenewalStartForms", type: :request do
             updated_tr_count = TransientRegistration.count
 
             expect(original_tr_count).to eq(updated_tr_count)
+          end
+
+          context "when the state is different" do
+            let(:transient_registration) do
+              create(:transient_registration,
+                     :has_required_data,
+                     account_email: user.email,
+                     workflow_state: "smart_answers_form")
+            end
+
+            let(:valid_params) { { reg_identifier: transient_registration.reg_identifier } }
+
+            it "returns a 302 response" do
+              post renewal_start_forms_path, renewal_start_form: valid_params
+              expect(response).to have_http_status(302)
+            end
+
+            it "redirects to the correct form" do
+              post renewal_start_forms_path, renewal_start_form: valid_params
+              expect(response).to redirect_to(new_smart_answers_form_path(valid_params[:reg_identifier]))
+            end
+
+            it "does not create a new transient registration" do
+              # Touch the test object so it gets created now and the count is correct
+              transient_registration.touch
+
+              original_tr_count = TransientRegistration.count
+              post renewal_start_forms_path, renewal_start_form: valid_params
+              updated_tr_count = TransientRegistration.count
+
+              expect(original_tr_count).to eq(updated_tr_count)
+            end
+          end
+        end
+      end
+
+      context "when the signed-in user does not own the registration" do
+        context "when no renewal is in progress" do
+          let(:registration) do
+            create(:registration,
+                   :has_required_data,
+                   :expires_soon,
+                   account_email: "not-#{user.email}")
+          end
+          let(:valid_params) { { reg_identifier: registration.reg_identifier } }
+
+          it "prevents access" do
+            post renewal_start_forms_path, renewal_start_form: valid_params
+            skip("don't know what this should do yet")
+          end
+        end
+
+        context "when a renewal is in progress" do
+          let(:transient_registration) do
+            create(:transient_registration,
+                   :has_required_data,
+                   account_email: "not-#{user.email}",
+                   workflow_state: "renewal_start_form")
+          end
+          let(:valid_params) { { reg_identifier: transient_registration.reg_identifier } }
+
+          it "prevents access" do
+            post renewal_start_forms_path, renewal_start_form: valid_params
+            skip("don't know what this should do yet")
           end
         end
       end
