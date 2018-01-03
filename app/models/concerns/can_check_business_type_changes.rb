e2624f7e485d5af42a0b831c68a766114577a723
irisfaraway <iris.faraway@gmail.com>
2018-01-03 08:54:03 +0000

    Add text to page, remove 'return's
    Still need to add a proper link to the new registration service.

--- app/models/concerns/can_check_business_type_changes.rb
+++ app/models/concerns/can_check_business_type_changes.rb
@@ -9,71 +9,71 @@ module CanCheckBusinessTypeChanges
       case old_type
       # If the old type is the same as the new type, no change has happened and it's valid
       when business_type
-        return true
+        true
 
       # If the old type is sole trader and the new type is different
       when "soleTrader"
         case business_type
         when "overseas"
-          return true
+          true
         else
-          return false
+          false
         end
 
       # If the old type is partnership and the new type is different
       when "partnership"
         case business_type
         when "limitedLiabilityPartnership"
-          return true
+          true
         when "overseas"
-          return true
+          true
         else
-          return false
+          false
         end
 
       # If the old type is limited company and the new type is different
       when "limitedCompany"
         case business_type
         when "limitedLiabilityPartnership"
-          return true
+          true
         when "overseas"
-          return true
+          true
         else
-          return false
+          false
         end
 
       # If the old type is public body and the new type is different
       when "publicBody"
         case business_type
         when "localAuthority"
-          return true
+          true
         else
-          return false
+          false
         end
 
       # If the old type is charity and the new type is different
       when "charity"
         case business_type
         when "other"
-          return true
+          true
         when "overseas"
-          return true
+          true
         else
-          return false
+          false
         end
 
       # If the old type is authority and the new type is different
       when "authority"
         case business_type
         when "foo"
-          return true
+          true
         else
-          return false
+          false
         end
 
       # If the old type is none of the above
       else
-        return false
+        false
       end
     end
   end
