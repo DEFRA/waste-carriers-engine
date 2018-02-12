30b06b94f1c6d97a60f95d43f52617f2b769f7c2
irisfaraway <iris.faraway@gmail.com>
2018-02-12 17:37:41 +0000

    Pre-select a previously chosen address
    If the user selects an address from the dropdown and saves it, this address should be pre-selected when they return to the page.
    
    Tests are still in a mess but should be fixed shortly!

--- app/forms/company_address_form.rb
+++ app/forms/company_address_form.rb
@@ -11,9 +11,8 @@ class CompanyAddressForm < BaseForm
     self.business_type = @transient_registration.business_type
     self.temp_postcode = @transient_registration.temp_postcode
 
-    return unless temp_postcode.present?
-    address_finder = AddressFinderService.new(temp_postcode)
-    self.temp_addresses = address_finder.search_by_postcode
+    look_up_addresses
+    preselect_existing_address
   end
 
   def submit(params)
@@ -28,11 +27,28 @@ class CompanyAddressForm < BaseForm
 
   private
 
+  # Look up addresses based on the temp_postcode
+  def look_up_addresses
+    return unless temp_postcode.present?
+    address_finder = AddressFinderService.new(temp_postcode)
+    self.temp_addresses = address_finder.search_by_postcode
+  end
+
+  # If an address has already been assigned to the transient registration, pre-select it
+  def preselect_existing_address
+    return unless @transient_registration.addresses.present?
+    current_address = @transient_registration.addresses.where(address_type: "REGISTERED").first
+    return unless current_address.uprn.present?
+    selected_address = temp_addresses.detect { |address| address["uprn"] == current_address.uprn.to_s }
+    self.address = selected_address["uprn"]
+  end
+
   def add_address(selected_address_uprn)
     return if selected_address_uprn.blank?
 
     data = temp_addresses.detect { |address| address["uprn"] == selected_address_uprn }
     address = Address.create_from_os_places_data(data)
     address.assign_attributes(address_type: "REGISTERED")
+    address
   end
 end
