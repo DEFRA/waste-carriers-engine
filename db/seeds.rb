545b39dbbe5ef782ebc4e5a79f9c15f9577319bc
irisfaraway <iris.faraway@gmail.com>
2018-02-26 13:51:00 +0000

    Generate dates with "date_flag"
    When we parse each JSON seed, check to see if the "date_flag" is set and then generate all time-sensitive dates based on the flag. Options are expired, in the renewal window or outside the renewal window.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# This file adds seeds by parsing the JSON files in the db/seeds/ folder.
+
+# If a "date_flag" is set for a seed, we generate the dates dynamically.
+# This ensures a seed which is always intended to be in a time-sensitive state
+# remains in that state without having to manually update the dates.
+
+def registered_date(flag)
+  dates = {
+    expired: 37.months.ago,
+    in_renewal_window: 35.months.ago,
+    outside_renewal_window: 12.months.ago
+  }
+
+  dates[flag.to_sym] || Date.today
+end
+
+def parse_dates(seed, date)
+  seed["metaData"]["dateRegistered"] = date
+  seed["metaData"]["lastModified"] = date
+  seed["metaData"]["dateActivated"] = date
+  seed["convictionSearchResult"]["searchedAt"] = date
+  seed["expires_on"] = date + 3.years
+  seed["keyPeople"].each do |key_person|
+    key_person["convictionSearchResult"]["searchedAt"] = date
+  end
+end
 
 # Only seed if not running in production or we specifically require it, eg. for Heroku
 if !Rails.env.production? || ENV["WCR_ALLOW_SEED"]
@@ -16,6 +35,12 @@ if !Rails.env.production? || ENV["WCR_ALLOW_SEED"]
   seeds = []
   Dir.glob("#{Rails.root}/db/seeds/*.json").each do |file|
     seeds << JSON.parse(File.read(file))
+  end
+
+  seeds.each do |seed|
+    next unless seed["date_flag"].present?
+    parse_dates(seed, registered_date(seed["date_flag"]))
+    seed.delete("date_flag")
   end
 
   # Sort seeds to list ones with regIdentifiers first
