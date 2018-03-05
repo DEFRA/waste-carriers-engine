14fe41020b4d94e34996cdab1143bc15a9b69820
irisfaraway <iris.faraway@gmail.com>
2018-03-05 10:30:43 +0000

    Tweak validation handling and heading
    These changes are following the QA review. The bulk of the changes are about how we handle validation messages. This commit reduces the number of validation errors which a user can get for submitting the form without all the required information. Previously a totally blank form would trigger about 6 different messages - now it should just be one. Date validation errors have also been simplified so if all fields are missing, we only display a single message. We also only check that the complete date is a valid date once we've validated the individual fields to reduce the number of errors users get.
    
    Also changed the heading text for overseas users.

--- app/validators/date_of_birth_validator.rb
+++ app/validators/date_of_birth_validator.rb
@@ -1,19 +1,34 @@
 class DateOfBirthValidator < ActiveModel::Validator
   def validate(record)
+    # Make sure we have any date fields to validate before proceeding
+    return false if all_fields_empty?(record)
+
+    # Validate the content of individual fields
     fields = { day: record.dob_day, month: record.dob_month, year: record.dob_year }
 
+    all_fields_valid = true
     fields.each do |type, field|
-      validate_field(record, type, field)
+      next if field_is_valid?(record, type, field)
+      all_fields_valid = false
     end
 
+    # If individual fields are OK, check the validity of the date as a whole
+    return false unless all_fields_valid
     dob_is_a_date?(record)
   end
 
   private
 
-  def validate_field(record, type, field)
-    return unless field_present?(record, type, field)
-    return unless field_is_integer?(record, type, field)
+  def all_fields_empty?(record)
+    fields = [record.dob_day, record.dob_month, record.dob_year].compact
+    return false if fields.any?
+    record.errors.add(:date_of_birth, :not_a_date)
+    true
+  end
+
+  def field_is_valid?(record, type, field)
+    return false unless field_present?(record, type, field)
+    return false unless field_is_integer?(record, type, field)
     field_is_in_correct_range?(record, type, field)
   end
 
