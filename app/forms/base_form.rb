aab0f8bd3dc1bd287e8d69e618f9336af341a2ae
irisfaraway <iris.faraway@gmail.com>
2018-04-05 10:44:31 +0100

    Strip whitespace on submitting a form

--- app/forms/base_form.rb
+++ app/forms/base_form.rb
@@ -12,6 +12,8 @@ class BaseForm
     # Additional attributes are set in individual form subclasses
     self.reg_identifier = reg_identifier
 
+    attributes = strip_excess_whitespace(attributes)
+
     # Update the transient registration with params from the registration if valid
     if valid?
       @transient_registration.update_attributes(attributes)
@@ -40,6 +42,32 @@ class BaseForm
     return if @transient_registration.valid?
     @transient_registration.errors.each do |_attribute, message|
       errors[:base] << message
+    end
+  end
+
+  # Whitespace stripping
+
+  def strip_excess_whitespace(attributes)
+    # Loop over each value and strip the whitespace, or strip the whitespace from values nested within it
+    attributes.each_pair do |_key, value|
+      strip_string(value) if value.is_a?(String)
+      strip_hash(value) if value.is_a?(Hash)
+      strip_array(value) if value.is_a?(Array)
+    end
+    attributes
+  end
+
+  def strip_string(string)
+    string.strip!
+  end
+
+  def strip_hash(hash)
+    strip_excess_whitespace(hash)
+  end
+
+  def strip_array(array)
+    array.each do |nested_object|
+      strip_excess_whitespace(nested_object.attributes)
     end
   end
 end
