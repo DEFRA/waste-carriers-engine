f329369b8858e8d16e094c704298004a67b30e25
irisfaraway <iris.faraway@gmail.com>
2018-04-10 12:00:42 +0100

    Refactor to inherit from AddressForm
    CompanyAddressForm and ContactAddressForm are very similar, so any shared methods should be inherited from the AddressForm instead.

--- app/forms/contact_address_form.rb
+++ app/forms/contact_address_form.rb
@@ -1,62 +1,25 @@
-class ContactAddressForm < BaseForm
-  attr_accessor :business_type
+class ContactAddressForm < AddressForm
   attr_accessor :temp_contact_postcode
-  attr_accessor :temp_addresses
-  attr_accessor :temp_address
-  attr_accessor :addresses
 
   def initialize(transient_registration)
     super
-    # We only use this for the correct microcopy
-    self.business_type = @transient_registration.business_type
     self.temp_contact_postcode = @transient_registration.temp_contact_postcode
 
     look_up_addresses
     preselect_existing_address
   end
 
-  def submit(params)
-    # Assign the params for validation and pass them to the BaseForm method for updating
-    self.addresses = add_or_replace_address(params[:temp_address])
-    attributes = { addresses: addresses }
-
-    super(attributes, params[:reg_identifier])
-  end
-
-  validates :addresses, presence: true
-
   private
 
-  # Look up addresses based on the temp_contact_postcode
-  def look_up_addresses
-    if temp_contact_postcode.present?
-      address_finder = AddressFinderService.new(temp_contact_postcode)
-      self.temp_addresses = address_finder.search_by_postcode
-    else
-      self.temp_addresses = []
-    end
+  def temp_postcode
+    temp_contact_postcode
   end
 
-  # If an address has already been assigned to the transient registration, pre-select it
-  def preselect_existing_address
-    return unless @transient_registration.addresses.present?
-    current_address = @transient_registration.contact_address
-    return unless current_address.uprn.present?
-    selected_address = temp_addresses.detect { |address| address["uprn"] == current_address.uprn.to_s }
-    self.temp_address = selected_address["uprn"] if selected_address.present?
+  def saved_address
+    @transient_registration.contact_address
   end
 
-  def add_or_replace_address(selected_address_uprn)
-    return if selected_address_uprn.blank?
-
-    data = temp_addresses.detect { |address| address["uprn"] == selected_address_uprn }
-    address = Address.create_from_os_places_data(data)
-    address.assign_attributes(address_type: "CONTACT")
-
-    # Update the transient object's nested addresses, replacing any existing registered address
-    updated_addresses = @transient_registration.addresses
-    updated_addresses.delete(@transient_registration.contact_address) if @transient_registration.contact_address
-    updated_addresses << address
-    updated_addresses
+  def address_type
+    "CONTACT"
   end
 end
