b6070ee52d84e64caadce8d2141d0011625e8e87
irisfaraway <iris.faraway@gmail.com>
2018-04-11 14:16:06 +0100

    Display error if renewal is missing some information
    Users shouldn't be able to get this far without answering all the required questions. However, if something has gone wrong, this error lists the missing fields and asks them to go back and fix it.

--- app/forms/check_your_answers_form.rb
+++ app/forms/check_your_answers_form.rb
@@ -1,18 +1,17 @@
 class CheckYourAnswersForm < BaseForm
   attr_accessor :business_type, :company_name, :company_no, :contact_address, :contact_email, :declared_convictions,
-                :first_name, :last_name, :location, :main_people, :phone_number, :registered_address,
-                :registration_type, :relevant_people, :tier
+                :contact_name, :location, :main_people, :phone_number, :registered_address, :registration_type,
+                :relevant_people, :tier
 
   def initialize(transient_registration)
     super
     self.business_type = @transient_registration.business_type
     self.company_name = @transient_registration.company_name
     self.company_no = @transient_registration.company_no
+    self.contact_name = format_contact_name
     self.contact_address = displayable_address(@transient_registration.contact_address)
     self.contact_email = @transient_registration.contact_email
-    self.declared_convictions = boolean_to_string(@transient_registration.declared_convictions)
-    self.first_name = @transient_registration.first_name
-    self.last_name = @transient_registration.last_name
+    self.declared_convictions = @transient_registration.declared_convictions
     self.location = @transient_registration.location
     self.main_people = @transient_registration.main_people
     self.phone_number = @transient_registration.phone_number
@@ -20,6 +19,8 @@ class CheckYourAnswersForm < BaseForm
     self.registration_type = @transient_registration.registration_type
     self.relevant_people = @transient_registration.relevant_people
     self.tier = @transient_registration.tier
+
+    required_fields_filled_in?
   end
 
   def submit(params)
@@ -28,19 +29,56 @@ class CheckYourAnswersForm < BaseForm
     super(attributes, params[:reg_identifier])
   end
 
-  def contact_name
-    "#{first_name} #{last_name}"
-  end
-
   def registration_type_changed?
     @transient_registration.registration_type_changed?
   end
 
+  def required_fields_filled_in?
+    valid = true
+    required_fields.each do |field|
+      # Field must be present or equal to 'false' to be valid
+      # This is to allow for booleans - false is fine, nil is not
+      next if send(field).present? || send(field).to_s == "false"
+      errors.add(field, :missing)
+      valid = false
+    end
+    valid
+  end
+
   private
 
-  def boolean_to_string(value)
-    return "yes" if value
-    "no"
+  # These are fields which must have been filled in by this point for this to be a valid renewal
+  def required_fields
+    fields = %i[business_type
+                company_name
+                contact_name
+                contact_address
+                contact_email
+                declared_convictions
+                location
+                main_people
+                phone_number
+                registered_address
+                registration_type
+                tier]
+    fields << :company_no if company_no_required?
+    fields << :relevant_people if relevant_people_required?
+
+    fields
+  end
+
+  def company_no_required?
+    return true if business_type == "limitedCompany"
+    return true if business_type == "limitedLiabilityPartnership"
+    false
+  end
+
+  def relevant_people_required?
+    declared_convictions
+  end
+
+  def format_contact_name
+    "#{@transient_registration.first_name} #{@transient_registration.last_name}"
   end
 
   def displayable_address(address)
