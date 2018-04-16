118ad6b541e32308f24dbb4b8d5a7f2e1f26d456
irisfaraway <iris.faraway@gmail.com>
2018-04-16 13:19:49 +0100

    Tests for ApplicationHelper, and fix survey link
    Added some tests for the ApplicationHelper. These will need improving but it's a start!
    
    Also fixed the link for the survey while I was in there as it was set to go to the wrong project.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def self.feedback_survey_url(current_title)
-    survey_url = "https://www.smartsurvey.co.uk/s/floodexemptions/?"
+    survey_url = "https://www.smartsurvey.co.uk/s/waste-carriers/?"
     survey_params = { referringpage: current_title }
     survey_url + survey_params.to_query
   end
