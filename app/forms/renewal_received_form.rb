4d3f837377bf8ec036e4545939a2052c44e58084
irisfaraway <iris.faraway@gmail.com>
2018-04-30 13:26:40 +0100

    Set up page layout and content

--- app/forms/renewal_received_form.rb
+++ app/forms/renewal_received_form.rb
@@ -1,6 +1,9 @@
 class RenewalReceivedForm < BaseForm
+  attr_accessor :contact_email
+
   def initialize(transient_registration)
     super
+    self.contact_email = @transient_registration.contact_email
   end
 
   # Override BaseForm method as users shouldn't be able to submit this form
