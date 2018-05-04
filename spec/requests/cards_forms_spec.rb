079832cecd1c111c4036185295100ef663c691b3
irisfaraway <iris.faraway@gmail.com>
2018-05-04 13:21:26 +0100

    Swap in shared examples for viewing forms
    Swap out existing tests to GET a form in favour of the new shared examples. Currently these all follow the 'locked-in' behaviour as explained above, although this will change in the future.
    
    The renewal start page behaves a bit differently so doesn't need the shared examples.

--- spec/requests/cards_forms_spec.rb
+++ spec/requests/cards_forms_spec.rb
@@ -1,42 +1,8 @@
 require "rails_helper"
+require "support/shared_examples/request_get_locked_in_form"
 
 RSpec.describe "CardsForms", type: :request do
-  describe "GET new_cards_path" do
-    context "when a valid user is signed in" do
-      let(:user) { create(:user) }
-      before(:each) do
-        sign_in(user)
-      end
-
-      context "when a valid transient registration exists" do
-        let(:transient_registration) do
-          create(:transient_registration,
-                 :has_required_data,
-                 account_email: user.email,
-                 workflow_state: "cards_form")
-        end
-
-        it "returns a success response" do
-          get new_cards_form_path(transient_registration[:reg_identifier])
-          expect(response).to have_http_status(200)
-        end
-      end
-
-      context "when a transient registration is in a different state" do
-        let(:transient_registration) do
-          create(:transient_registration,
-                 :has_required_data,
-                 account_email: user.email,
-                 workflow_state: "renewal_start_form")
-        end
-
-        it "redirects to the form for the current state" do
-          get new_cards_form_path(transient_registration[:reg_identifier])
-          expect(response).to redirect_to(new_renewal_start_form_path(transient_registration[:reg_identifier]))
-        end
-      end
-    end
-  end
+  include_examples "GET locked-in form", form = "cards_form"
 
   describe "POST cards_forms_path" do
     context "when a valid user is signed in" do
