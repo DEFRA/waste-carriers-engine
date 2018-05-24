7d78e11bd198acd95fcf798a61deccc2d47c4b84
irisfaraway <iris.faraway@gmail.com>
2018-05-24 17:14:36 +0100

    Tweak stubbing for Worldpay XML test
    The cost for orders and orderitems relies on environment variables, so for consistent testing it's better to just stub the value instead.

--- spec/services/worldpay_xml_service_spec.rb
+++ spec/services/worldpay_xml_service_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe WorldpayXmlService do
     create(:transient_registration,
            :has_required_data,
            :has_overseas_addresses,
-           :has_finance_details)
+           :has_finance_details,
+           temp_cards: 0)
   end
   let(:order) { transient_registration[:financeDetails][:orders].first }
   let(:worldpay_xml_service) { WorldpayXmlService.new(transient_registration, order) }
@@ -16,11 +17,10 @@ RSpec.describe WorldpayXmlService do
     transient_registration.reg_identifier = "CBDU9999"
 
     allow(Rails.configuration).to receive(:worldpay_merchantcode).and_return("MERCHANTCODE")
-    allow(Rails.configuration).to receive(:renewal_charge).and_return(100)
-    allow(Rails.configuration).to receive(:card_charge).and_return(5)
 
     # This is generated based on the time, so to avoid any millisecond malarky, let's just stub it
     allow_any_instance_of(Order).to receive(:order_code).and_return("1234567890")
+    allow_any_instance_of(Order).to receive(:total_amount).and_return(100)
   end
 
   describe "build_xml" do
