dbc49cf957312163dd512a4056a471f31bce8dd8
irisfaraway <iris.faraway@gmail.com>
2018-05-25 14:17:03 +0100

    Send initial request to WorldPay and parse the response
    Now that we have the XML, we can send it to WorldPay and get back a response. This contains the link we need for redirecting our users, as well as a reference ID.

--- app/services/worldpay_service.rb
+++ app/services/worldpay_service.rb
@@ -1,5 +1,54 @@
 class WorldpayService
-  def initialize(transient_registration)
+  def initialize(transient_registration, order)
     @transient_registration = transient_registration
+    @order = order
+    @url = "https://secure-test.worldpay.com/jsp/merchant/xml/paymentService.jsp"
+    @username = Rails.configuration.worldpay_username
+    @password = Rails.configuration.worldpay_password
+  end
+
+  def set_up_payment_link
+    response = send_request
+    reference = parse_response(response)
+    return :error unless reference.present?
+    reference[:link]
+  end
+
+  private
+
+  def send_request
+    xml_service = WorldpayXmlService.new(@transient_registration, @order)
+    xml = xml_service.build_xml
+
+    Rails.logger.debug "Sending initial request to WorldPay"
+
+    begin
+      response = RestClient::Request.execute(
+        method: :get,
+        url: @url,
+        payload: xml,
+        headers: {
+          "Authorization" => "Basic " + Base64.encode64(@username + ":" + @password).to_s
+        }
+      )
+
+      Rails.logger.debug "Received response from WorldPay"
+      response
+    end
+  end
+
+  def parse_response(response)
+    doc = Nokogiri::XML(response)
+    reference = doc.at_xpath("//reference")
+
+    if reference.present?
+      reference_id = reference.attribute("id").text
+      reference_link = reference.text
+
+      { id: reference_id, link: reference_link }
+    else
+      Rails.logger.error "Could not parse Worldpay response: #{response}"
+      return nil
+    end
   end
 end
--- spec/services/worldpay_service_spec.rb
+++ spec/services/worldpay_service_spec.rb
@@ -1,4 +1,34 @@
 require "rails_helper"
 
 RSpec.describe WorldpayService do
+  let(:transient_registration) do
+    create(:transient_registration,
+           :has_required_data,
+           :has_overseas_addresses,
+           :has_finance_details,
+           temp_cards: 0)
+  end
+  let(:order) { transient_registration[:financeDetails][:orders].first }
+  let(:worldpay_service) { WorldpayService.new(transient_registration, order) }
+
+  describe "set_up_payment_link" do
+    it "returns a link" do
+      VCR.use_cassette("worldpay_initial_request") do
+        link = "https://secure-test.worldpay.com/wcc/dispatcher?OrderKey="
+        expect(worldpay_service.set_up_payment_link).to include(link)
+      end
+    end
+
+    context "when the request is invalid" do
+      before do
+        allow_any_instance_of(WorldpayXmlService).to receive(:build_xml).and_return("foo")
+      end
+
+      it "returns :error" do
+        VCR.use_cassette("worldpay_initial_request_invalid") do
+          expect(worldpay_service.set_up_payment_link).to eq(:error)
+        end
+      end
+    end
+  end
 end
