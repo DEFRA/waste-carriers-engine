c46ab05391f53b848ead2fce128aecf7169c964f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-06-25 23:21:40 +0100

    Standardise Services domain address
    This changes updates the name of the env var that holds the address of the Services, making it consistent across all projects.

--- config/application.rb
+++ config/application.rb
@@ -55,8 +55,8 @@ module WasteCarriersRenewals
     # Paths
     config.wcrs_renewals_url = ENV["WCRS_RENEWALS_DOMAIN"] || "http://localhost:3002"
     config.wcrs_frontend_url = ENV["WCRS_FRONTEND_DOMAIN"] || "http://localhost:3000"
-    config.wcrs_services_url = ENV["WCRS_SERVICES_PUBLIC_APP_DOMAIN"] || "http://localhost:8003"
-    config.os_places_service_url = ENV["WCRS_OS_PLACES_DOMAIN"]       || "http://localhost:8005"
+    config.wcrs_services_url = ENV["WCRS_SERVICES_DOMAIN"] || "http://localhost:8003"
+    config.os_places_service_url = ENV["WCRS_OS_PLACES_DOMAIN"] || "http://localhost:8005"
 
     # Fees
     config.renewal_charge = ENV["WCRS_RENEWAL_CHARGE"].to_i || 0
