6f47be7fb6efd9cfde2b87651895f640d097dde5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-06-25 21:48:10 +0100

    Standardise OS Places lookup domain address
    This changes updates the name of the env var that holds the address of the OS Places Address lookup service, making it consistent across all projects.

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module WasteCarriersRenewals
     config.wcrs_renewals_url = ENV["WCRS_RENEWALS_PUBLIC_APP_DOMAIN"] || "http://localhost:3002"
     config.wcrs_frontend_url = ENV["WCRS_FRONTEND_PUBLIC_APP_DOMAIN"] || "http://localhost:3000"
     config.wcrs_services_url = ENV["WCRS_SERVICES_PUBLIC_APP_DOMAIN"] || "http://localhost:8003"
-    config.os_places_service_url = ENV["WCRS_RENEWALS_OS_PLACES_SERVICE_DOMAIN"] || "http://localhost:8005"
+    config.os_places_service_url = ENV["WCRS_OS_PLACES_DOMAIN"]       || "http://localhost:8005"
 
     # Fees
     config.renewal_charge = ENV["WCRS_RENEWAL_CHARGE"].to_i || 0
