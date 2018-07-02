134ae0e6550d5654859a99f220b0316a832c3251
irisfaraway <iris.faraway@gmail.com>
2018-07-02 12:43:57 +0100

    Define current_ability in ApplicationController
    This fixes lots of the CanCanCan errors as it now correctly instantiates the Ability. Back down to 6 test failures now.

--- app/controllers/waste_carriers_engine/application_controller.rb
+++ app/controllers/waste_carriers_engine/application_controller.rb
@@ -3,5 +3,11 @@ module WasteCarriersEngine
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
+
+    private
+
+    def current_ability
+      @current_ability ||= Ability.new(current_user)
+    end
   end
 end
