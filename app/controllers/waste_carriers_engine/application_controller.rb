07a80ccfccd4087670d3fe7ccbd1fe0335d779e9
irisfaraway <iris.faraway@gmail.com>
2018-07-02 18:31:56 +0100

    Engine uses host app layout and custom Devise views
    When the engine is mounted in a host app, it should use that app's default layout.
    
    We also move the Devise views back up a level, as otherwise the custom views aren't used in the host app.

--- app/controllers/waste_carriers_engine/application_controller.rb
+++ app/controllers/waste_carriers_engine/application_controller.rb
@@ -4,6 +4,9 @@ module WasteCarriersEngine
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
 
+    # Use the host application's default layout
+    layout "application"
+
     private
 
     def current_ability
