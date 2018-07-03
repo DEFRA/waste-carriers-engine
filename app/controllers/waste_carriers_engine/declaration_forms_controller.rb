152878f7fa472a94716defc6531a955046d8bf48
irisfaraway <iris.faraway@gmail.com>
2018-07-03 15:05:20 +0100

    Fix missing namespace during conviction checks

--- app/controllers/waste_carriers_engine/declaration_forms_controller.rb
+++ app/controllers/waste_carriers_engine/declaration_forms_controller.rb
@@ -7,7 +7,7 @@ module WasteCarriersEngine
     def create
       return unless super(DeclarationForm, "declaration_form")
 
-      entity_matching_service = EntityMatchingService.new(@transient_registration)
+      entity_matching_service = WasteCarriersEngine::EntityMatchingService.new(@transient_registration)
       entity_matching_service.check_business_for_matches
       entity_matching_service.check_people_for_matches
     end
