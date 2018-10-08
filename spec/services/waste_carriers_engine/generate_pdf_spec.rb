7906e807d5ae9d05baaa8e2aa29a5d600da40cbc
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-10-08 22:40:53 +0100

    Updated version of `GeneratePdfService`
    This is a cleaned up version of the GeneratePdfService, though I am torn over the need to have it.
    
    As a separate service it means we can have a unit test that ensure at a basic level we can generate pdf's, without having to go through the mailer to generate one. However it doesn't do very much at the moment, and the line in the initializer could just as easily be copied into the mailer itself.
    
    The alternative is to move the `render_to_string()` call currently sat in the renewal mailer into the service. However to do this the service then needs to know about ActionView and rendering, which already sits in the mailer.
    
    So for now leaving as this till I have reached a conclusion.

--- spec/services/waste_carriers_engine/generate_pdf_spec.rb
+++ spec/services/waste_carriers_engine/generate_pdf_spec.rb
@@ -2,14 +2,23 @@ require "rails_helper"
 
 module WasteCarriersEngine
   RSpec.describe GeneratePdfService do
-    context "testing" do
-      it "has the right config" do
-        expect(WickedPdf.config[:exe_path]).to eq("#{Dir.pwd}/script/wkhtmltopdf.sh")
-      end
-      it "can generate a pdf" do
-        pdf = WickedPdf.new.pdf_from_string('<h1>Hello There!</h1>')
-        expect(pdf).not_to eq(nil)
+    context "when initialized with a string" do
+      let(:generate_pdf_service) { GeneratePdfService.new("<h1>Hello There!</h1>") }
+
+      it "generates a pdf" do
+        # There doesn't appear to be any special way to confirm its a PDF other
+        # than checking the string returned starts with this
+        expect(generate_pdf_service.pdf).to start_with("%PDF-")
       end
     end
+
+    context "when initialized with null" do
+      let(:generate_pdf_service) { GeneratePdfService.new(nil) }
+
+      it "does not generate a pdf" do
+        expect(generate_pdf_service.pdf).to be_nil
+      end
+    end
+
   end
 end
