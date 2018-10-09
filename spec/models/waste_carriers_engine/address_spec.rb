82faec9e3a8ebb4835b643bef158e1295a7361e3
irisfaraway <iris.faraway@gmail.com>
2018-10-09 15:49:01 +0100

    Amend spec to match desired result
    The `address_line` fields should be built using the `buildingNumber` and `lines` provided by OS Places.

--- spec/models/waste_carriers_engine/address_spec.rb
+++ spec/models/waste_carriers_engine/address_spec.rb
@@ -8,30 +8,51 @@ module WasteCarriersEngine
       context "when it is given address data" do
         let(:data) do
           {
-            "organisationName" => "Foo Industries",
-            "subBuildingName" => "Suite 5",
-            "buildingName" => "The Bar Building",
+            "buildingName" => "Foo Buildings",
             "buildingNumber" => "5",
-            "thoroughfareName" => "Baz Street"
+            "dependentLocality" => "Baz Village",
+            "thoroughfareName" => "Bar Street",
+            "lines" => [
+              "FOO BUILDINGS",
+              "BAR STREET",
+              "BAZ VILLAGE"
+            ]
           }
         end
 
         it "should assign the correct address lines" do
           address.assign_address_lines(data)
-          expect(address[:address_line_1]).to eq("Foo Industries")
-          expect(address[:address_line_2]).to eq("Suite 5")
-          expect(address[:address_line_3]).to eq("The Bar Building")
-          expect(address[:address_line_4]).to eq("5 Baz Street")
+          expect(address[:address_line_1]).to eq("5")
+          expect(address[:address_line_2]).to eq("FOO BUILDINGS")
+          expect(address[:address_line_3]).to eq("BAR STREET")
+          expect(address[:address_line_4]).to eq("BAZ VILLAGE")
         end
 
-        context "when not all address fields are used" do
-          before { data["buildingName"] = "" }
+        context "when the buildingNumber is not used" do
+          before do
+            data.delete("buildingNumber")
+          end
 
           it "should skip blank fields when assigning lines" do
             address.assign_address_lines(data)
-            expect(address[:address_line_1]).to eq("Foo Industries")
-            expect(address[:address_line_2]).to eq("Suite 5")
-            expect(address[:address_line_3]).to eq("5 Baz Street")
+            expect(address[:address_line_1]).to eq("FOO BUILDINGS")
+            expect(address[:address_line_2]).to eq("BAR STREET")
+            expect(address[:address_line_3]).to eq("BAZ VILLAGE")
+            expect(address[:address_line_4].present?).to eq(false)
+          end
+        end
+
+        context "when the lines are not all used" do
+          before do
+            data["lines"] = ["FOO BUILDINGS", "BAR STREET"]
+          end
+
+          it "should skip blank fields when assigning lines" do
+            address.assign_address_lines(data)
+            expect(address[:address_line_1]).to eq("5")
+            expect(address[:address_line_2]).to eq("FOO BUILDINGS")
+            expect(address[:address_line_3]).to eq("BAR STREET")
+            expect(address[:address_line_4].present?).to eq(false)
           end
         end
       end
