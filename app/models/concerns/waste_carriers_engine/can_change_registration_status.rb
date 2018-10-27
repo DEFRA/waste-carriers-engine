9932ac613823b67f69986547844db4b2e2968881
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-10-27 23:40:05 +0100

    Add implementation for grace window
    Update the `CanChangeRegistrationStatus` concern so that it knows about the grace window and uses this knowledge in its determination as to whether something can be renewed.

--- app/models/concerns/waste_carriers_engine/can_change_registration_status.rb
+++ app/models/concerns/waste_carriers_engine/can_change_registration_status.rb
@@ -76,12 +76,11 @@ module WasteCarriersEngine
     # Guards
     def renewal_allowed?
       return true if renewal_application_submitted?
+      return true if close_to_expiry_date? && should_not_be_expired?
 
-      # The only time an expired registration can be renewed is if the application has previously been submitted -
-      # otherwise expiry is an automatic no
+      # The only time an expired registration can be renewed is if the application has previously been submitted,
+      # or it is withion the grace window - otherwise expiry is an automatic no
       return false if EXPIRED?
-
-      close_to_expiry_date? && should_not_be_expired?
     end
 
     def renewal_application_submitted?
@@ -100,7 +99,22 @@ module WasteCarriersEngine
       expiry_day = expiry_time_adjusted_for_daylight_savings.to_date
       # We store dates and times in UTC, but want to use the current date in the UK, not necessarily UTC
       current_day = Time.now.in_time_zone("London").to_date
+
+      return true if in_expiry_grace_window?(expiry_day, current_day)
+
       current_day < expiry_day
+    end
+
+    # Its important to note that a registration is expired on its expires_on date.
+    # For example if the expires_on date is Oct 1, then the registration was
+    # ACTIVE Sept 30, and EXPIRED Oct 1. If the grace window is 3 days, just
+    # adding 3 days to that date would give the impression the grace window lasts
+    # till Oct 4 (i.e. 1 + 3) when in fact we need to include the 1st as one of
+    # our grace window days.
+    def in_expiry_grace_window?(expiry_date, current_day)
+      return false if current_day > ((expiry_date + Rails.configuration.grace_window) - 1.day)
+
+      true
     end
 
     # expires_on is stored as a Time in UTC and then converted to a Date.
