45ee7c8c06ea19fab3f7d14b37d20efa474e6a09
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-11-06 16:44:40 +0000

    Fix CodeClimate Cognitive Complexity issue

--- app/models/concerns/waste_carriers_engine/can_change_registration_status.rb
+++ app/models/concerns/waste_carriers_engine/can_change_registration_status.rb
@@ -77,13 +77,17 @@ module WasteCarriersEngine
     def renewal_allowed?
       return false unless %w[ACTIVE EXPIRED].include?(status)
       return true if renewal_application_submitted?
+      return true if renew_expired_registration?
+
+      close_to_expiry_date? && should_not_be_expired?
+    end
+
+    def renew_expired_registration?
       return true if in_expiry_grace_window?
 
       # The only time an expired registration can be renewed is if the application has previously been submitted,
       # or it is withion the grace window - otherwise expiry is an automatic no
       return false if EXPIRED?
-
-      close_to_expiry_date? && should_not_be_expired?
     end
 
     def renewal_application_submitted?
