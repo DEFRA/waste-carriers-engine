88eb1a7e2af74316dec05f51a5ffcf4f60ee4d8d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-11-13 14:54:38 +0000

    Copy expiry date service from frontend
    Adding the `ExpiryDateService` to the project as we only want to bother checking if the declaration has been confirmed in `CanChangeRegistrationStatus.renewal_allowed?` if the registration is expired. At the moment the check is just looking at the status, but we also need to check the date as well hence the need to bring this service across to this project.

---
+++ app/services/waste_carriers_engine/expiry_date_service.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+module WasteCarriersEngine
+  # Contains methods related to dealing with dates in the service, for example
+  # whether a date would be considered as expired.
+  class ExpiryDateService
+    attr_reader :expiry_date
+
+    def initialize(expiry_date)
+      return if default_source_date(expiry_date)
+
+      return if date_from_utc_milliseconds(expiry_date)
+
+      @expiry_date = expiry_date.to_date
+    end
+
+    # For more details about the renewal window check out
+    # https://github.com/DEFRA/waste-carriers-renewals/wiki/Renewal-window
+    def date_can_renew_from
+      (@expiry_date - Rails.configuration.renewal_window.months)
+    end
+
+    def expired?
+      # Registrations are expired on the date recorded for their expiry date e.g.
+      # an expiry date of Mar 25 2018 means the registration was active up till
+      # 24:00 on Mar 24 2018.
+      return false if @expiry_date > Date.today
+
+      true
+    end
+
+    def in_renewal_window?
+      # If the registration expires in more than x months from now, its outside
+      # the renewal window
+      return true if @expiry_date < Rails.configuration.renewal_window.months.from_now
+
+      false
+    end
+
+    # Its important to note that a registration is expired on its expires_on date.
+    # For example if the expires_on date is Oct 1, then the registration was
+    # ACTIVE Sept 30, and EXPIRED Oct 1. If the grace window is 3 days, just
+    # adding 3 days to that date would give the impression the grace window lasts
+    # till Oct 4 (i.e. 1 + 3) when in fact we need to include the 1st as one of
+    # our grace window days.
+    def in_expiry_grace_window?
+      current_day = Date.today
+      last_day_of_grace_window = (@expiry_date + Rails.configuration.grace_window.days) - 1.day
+
+      current_day >= @expiry_date && current_day <= last_day_of_grace_window
+    end
+
+    private
+
+    # Believe we have functionality that relies on setting dates to the epoch
+    # where no existing date exists hence this bit of functionality
+    def default_source_date(provided_date)
+      return false unless provided_date.nil?
+
+      @expiry_date = Date.new(1970, 1, 1)
+
+      true
+    end
+
+    def date_from_utc_milliseconds(milliseconds)
+      return false if milliseconds.is_a? Date
+      return false if milliseconds.is_a? Time
+
+      milliseconds = milliseconds.to_i if milliseconds.is_a? String
+
+      @expiry_date = Time.at(milliseconds / 1000.0).to_date
+
+      true
+    end
+  end
+end
---
+++ spec/services/waste_carriers_engine/expiry_date_service_spec.rb
@@ -0,0 +1,171 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteCarriersEngine
+  RSpec.describe ExpiryDateService do
+    describe "#attributes" do
+
+      context "when initialized with a date" do
+        expiry_date = Date.today
+        subject { ExpiryDateService.new(expiry_date) }
+
+        it "sets :expiry_date to that date" do
+          expect(subject.expiry_date).to eq(expiry_date)
+        end
+      end
+
+      context "when initialized with nil" do
+        subject { ExpiryDateService.new(nil) }
+
+        it "sets :expiry_date to the epoch" do
+          expect(subject.expiry_date).to eq(Date.new(1970, 1, 1))
+        end
+      end
+
+      context "when initialized with UTC time in milliseconds" do
+        # Equivalent to 2018-03-25 14:35:27
+        subject { ExpiryDateService.new(152_198_492_700_0) }
+
+        it "sets :expiry_date to the Mar 25 2018" do
+          expect(subject.expiry_date).to eq(Date.new(2018, 3, 25))
+        end
+      end
+
+      context "when initialized with a string that represents a UTC time in milliseconds" do
+        # Equivalent to 2018-03-25 14:35:27
+        subject { ExpiryDateService.new("1521984927000") }
+
+        it "sets :expiry_date to the Mar 25 2018" do
+          expect(subject.expiry_date).to eq(Date.new(2018, 3, 25))
+        end
+      end
+    end
+
+    describe "#date_can_renew_from" do
+
+      context "when the renewal window is 3 months and the date provided is 2018-03-25" do
+        before do
+          allow(Rails.configuration).to receive(:renewal_window).and_return(3)
+        end
+
+        subject { ExpiryDateService.new(Date.parse("2018-03-25-T12:00:00.000Z")) }
+
+        it "returns a date of 2017-12-25" do
+          expect(subject.date_can_renew_from).to eq(Date.new(2017, 12, 25))
+        end
+      end
+
+    end
+
+    describe "#expired?" do
+
+      context "when the expiry date is yesterday" do
+        subject { ExpiryDateService.new(Date.yesterday) }
+
+        it "should be expired" do
+          expect(subject.expired?).to eq(true)
+        end
+      end
+
+      context "when the expiry date is today" do
+        subject { ExpiryDateService.new(Date.today) }
+
+        it "should be expired" do
+          expect(subject.expired?).to eq(true)
+        end
+      end
+
+      context "when the expiry date is tomorrow" do
+        subject { ExpiryDateService.new(Date.tomorrow) }
+
+        it "should not be expired" do
+          expect(subject.expired?).to eq(false)
+        end
+      end
+
+    end
+
+    describe "#in_renewal_window?" do
+
+      context "when the renewal window is 3 months" do
+        before do
+          allow(Rails.configuration).to receive(:renewal_window).and_return(3)
+        end
+
+        context "and the expiry date is 3 months and 2 days from today" do
+          subject { ExpiryDateService.new(3.months.from_now + 2.day) }
+
+          it "should not be in the window" do
+            expect(subject.in_renewal_window?).to eq(false)
+          end
+        end
+
+        context "and the expiry date is 3 months and 1 day from today" do
+          subject { ExpiryDateService.new(3.months.from_now + 1.day) }
+
+          it "should not be in the window" do
+            expect(subject.in_renewal_window?).to eq(false)
+          end
+        end
+
+        context "and the expiry date is 3 months from today" do
+          subject { ExpiryDateService.new(3.months.from_now) }
+
+          it "should be in the window" do
+            expect(subject.in_renewal_window?).to eq(true)
+          end
+        end
+
+        context "and the expiry date is less than 3 months from today" do
+          subject { ExpiryDateService.new(3.months.from_now - 1.day) }
+
+          it "should be in the window" do
+            expect(subject.in_renewal_window?).to eq(true)
+          end
+        end
+      end
+    end
+
+    describe "#in_expiry_grace_window?" do
+      context "when the grace window is 3 days" do
+        before { allow(Rails.configuration).to receive(:grace_window).and_return(3) }
+
+        let(:expires_on) { Date.today }
+        subject { ExpiryDateService.new(expires_on) }
+
+        context "and the current date is within the window" do
+          it "returns true" do
+            date_inside_grace_window = (expires_on + 3.days) - 1.day
+            Timecop.freeze(date_inside_grace_window) do
+              expect(subject.in_expiry_grace_window?).to eq(true)
+            end
+          end
+        end
+
+        context "and the current date is outside the window" do
+          it "returns false" do
+            date_outside_grace_window = (expires_on + 3.days)
+            Timecop.freeze(date_outside_grace_window) do
+              expect(subject.in_expiry_grace_window?).to eq(false)
+            end
+          end
+        end
+      end
+
+      context "when there is no grace window" do
+        before { allow(Rails.configuration).to receive(:grace_window).and_return(0) }
+
+        let(:expires_on) { Date.today }
+        subject { ExpiryDateService.new(expires_on) }
+
+        it "returns false" do
+          date_outside_grace_window = (expires_on + 3.days)
+          Timecop.freeze(date_outside_grace_window) do
+            expect(subject.in_expiry_grace_window?).to eq(false)
+          end
+        end
+      end
+    end
+  end
+end
