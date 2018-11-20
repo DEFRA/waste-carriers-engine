f470094e9dd392d68d1c0d818ec04408c6100d1d
irisfaraway <iris.faraway@gmail.com>
2018-11-20 18:15:46 +0000

    Add frozen_string_literal: true comments
    If I touch it, I fix it.

--- app/models/waste_carriers_engine/conviction_sign_off.rb
+++ app/models/waste_carriers_engine/conviction_sign_off.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module WasteCarriersEngine
   class ConvictionSignOff
     include Mongoid::Document
--- spec/factories/conviction_sign_off.rb
+++ spec/factories/conviction_sign_off.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :conviction_sign_off, class: WasteCarriersEngine::ConvictionSignOff do
     confirmed { "no" }
