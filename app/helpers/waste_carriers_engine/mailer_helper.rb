6be6119d7de99c216e0abaf34458fb01cfb05e46
irisfaraway <iris.faraway@gmail.com>
2018-12-10 14:13:36 +0000

    Use single-line syntax for unless

--- app/helpers/waste_carriers_engine/mailer_helper.rb
+++ app/helpers/waste_carriers_engine/mailer_helper.rb
@@ -11,9 +11,7 @@ module WasteCarriersEngine
 
       full_path = Rails.root.join(path)
 
-      unless File.exist?(full_path)
-        full_path = "#{Gem.loaded_specs['waste_carriers_engine'].full_gem_path}#{path}"
-      end
+      full_path = "#{Gem.loaded_specs['waste_carriers_engine'].full_gem_path}#{path}" unless File.exist?(full_path)
 
       attachments[image] = File.read(full_path)
       image_tag(attachments[image].url, **options)
