5818be0fb25971c76ebc6899f42e50a3d10438fb
irisfaraway <iris.faraway@gmail.com>
2019-03-20 16:33:37 +0000

    Use double quotes

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
 require "active_model/railtie"
 require "action_controller/railtie"
@@ -20,7 +20,7 @@ module WasteCarriersRenewals
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'UTC'
+    config.time_zone = "UTC"
 
     # The default locale is :en and all translations from config/locales/*/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
-  config.static_cache_control = 'public, max-age=3600'
+  config.static_cache_control = "public, max-age=3600"
 
   # Show error pages and disable caching.
   config.consider_all_requests_local       = false
