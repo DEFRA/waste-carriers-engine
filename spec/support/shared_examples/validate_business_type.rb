e2b946323ccba8316cfdaffc98c480d31bb3e0ca
irisfaraway <iris.faraway@gmail.com>
2019-08-20 13:26:08 +0100

    Remove unused code and YAML

--- spec/support/shared_examples/validate_business_type.rb
+++ spec/support/shared_examples/validate_business_type.rb
@@ -8,34 +8,4 @@ RSpec.shared_examples "validate business_type" do |form_factory|
     expect(validators[:business_type].first.class)
       .to eq(DefraRuby::Validators::BusinessTypeValidator)
   end
-
-  context "when a valid transient registration exists" do
-    let(:form) { build(form_factory, :has_required_data) }
-
-    context "when a business_type meets the requirements" do
-      it "is valid" do
-        expect(form).to be_valid
-      end
-    end
-
-    context "when a business_type is blank" do
-      before(:each) do
-        form.business_type = ""
-      end
-
-      it "is not valid" do
-        expect(form).to_not be_valid
-      end
-    end
-
-    context "when a business_type is not in the allowed list" do
-      before(:each) do
-        form.business_type = "foo"
-      end
-
-      it "is not valid" do
-        expect(form).to_not be_valid
-      end
-    end
-  end
 end
