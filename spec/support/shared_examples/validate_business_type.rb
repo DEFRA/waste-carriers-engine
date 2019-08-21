5efad734da00e5c2ae991951980ba5cf3d09d615
Iris Faraway <iris.faraway@gmail.com>
2019-08-21 13:14:07 +0100

    Fix spec text
    Co-Authored-By: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- spec/support/shared_examples/validate_business_type.rb
+++ spec/support/shared_examples/validate_business_type.rb
@@ -2,7 +2,7 @@
 
 # Tests for fields using the BusinessTypeValidator
 RSpec.shared_examples "validate business_type" do |form_factory|
-  it "validates the phone number using the BusinessTypeValidator class" do
+  it "validates the business type using the BusinessTypeValidator class" do
     validators = build(form_factory, :has_required_data)._validators
     expect(validators.keys).to include(:business_type)
     expect(validators[:business_type].first.class)
