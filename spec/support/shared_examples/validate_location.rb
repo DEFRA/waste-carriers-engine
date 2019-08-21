2522217c69ad1982f36439ea4bc2dbf9265f3d02
irisfaraway <iris.faraway@gmail.com>
2019-08-21 13:25:37 +0100

    Use shared location validator
    This PR removes our built-in location validation in favour of the shared validator in defra-ruby-validators.

--- spec/support/shared_examples/validate_location.rb
+++ spec/support/shared_examples/validate_location.rb
@@ -2,6 +2,13 @@
 
 # Tests for fields using the LocationValidator
 RSpec.shared_examples "validate location" do |form_factory|
+  it "validates the location using the LocationValidator class" do
+    validators = build(form_factory, :has_required_data)._validators
+    expect(validators.keys).to include(:location)
+    expect(validators[:location].first.class)
+      .to eq(DefraRuby::Validators::LocationValidator)
+  end
+
   context "when a valid transient registration exists" do
     let(:form) { build(form_factory, :has_required_data) }
 
