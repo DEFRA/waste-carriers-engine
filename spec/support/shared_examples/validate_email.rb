54cdcd5922c973762fbb7373f52d1d8ee1c89a2f
irisfaraway <iris.faraway@gmail.com>
2019-08-21 14:33:29 +0100

    Use shared email validator
    This PR removes our built-in email validation in favour of the shared validator in defra-ruby-validators.

--- spec/support/shared_examples/validate_email.rb
+++ spec/support/shared_examples/validate_email.rb
@@ -2,6 +2,13 @@
 
 # Tests for fields using the EmailValidator
 RSpec.shared_examples "validate email" do |form_factory, field|
+  it "validates the #{field} using the EmailValidator class" do
+    validators = build(form_factory, :has_required_data)._validators
+    expect(validators.keys).to include(field)
+    expect(validators[field].first.class)
+      .to eq(DefraRuby::Validators::EmailValidator)
+  end
+
   context "when a valid transient registration exists" do
     let(:form) { build(form_factory, :has_required_data) }
 
