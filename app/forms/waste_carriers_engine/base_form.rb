97258bf2fe1fa0b098ed250dbe55968aa5514e43
cintamani <cintamani.puddu@gmail.com>
2019-10-08 14:33:01 +0100

    Fix rubocop

--- app/forms/waste_carriers_engine/base_form.rb
+++ app/forms/waste_carriers_engine/base_form.rb
@@ -30,7 +30,7 @@ module WasteCarriersEngine
     end
 
     # TODO: Remove `reg_identifier` param
-    def submit(attributes, _reg_identifier=nil)
+    def submit(attributes, _reg_identifier = nil)
       attributes = strip_whitespace(attributes)
 
       transient_registration.assign_attributes(attributes)
