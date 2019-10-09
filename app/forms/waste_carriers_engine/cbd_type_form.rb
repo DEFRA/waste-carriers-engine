9c35bff8b025457082385285993b30763f84a85f
cintamani <cintamani.puddu@gmail.com>
2019-10-09 11:54:44 +0100

    Forms Refactoring - CBD type form
    From: https://eaflood.atlassian.net/browse/RUBY-693
    
    Update CBD type form to use strong parameters and delegate strategy

--- app/forms/waste_carriers_engine/cbd_type_form.rb
+++ app/forms/waste_carriers_engine/cbd_type_form.rb
@@ -2,20 +2,7 @@
 
 module WasteCarriersEngine
   class CbdTypeForm < BaseForm
-    attr_accessor :registration_type
-
-    def initialize(transient_registration)
-      super
-      self.registration_type = @transient_registration.registration_type
-    end
-
-    def submit(params)
-      # Assign the params for validation and pass them to the BaseForm method for updating
-      self.registration_type = params[:registration_type]
-      attributes = { registration_type: registration_type }
-
-      super(attributes, params[:reg_identifier])
-    end
+    delegate :registration_type, to: :transient_registration
 
     validates :registration_type, "waste_carriers_engine/registration_type": true
   end
