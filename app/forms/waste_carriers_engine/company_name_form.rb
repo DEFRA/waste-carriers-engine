c50e1537fe2ed2a7eac53c0680896077bf7f6915
cintamani <cintamani.puddu@gmail.com>
2019-10-09 12:42:22 +0100

    Forms refactoring - Company name form
    From: https://eaflood.atlassian.net/browse/RUBY-693
    
    Use delegate and strong parameter strategy on Company Name form

--- app/forms/waste_carriers_engine/company_name_form.rb
+++ app/forms/waste_carriers_engine/company_name_form.rb
@@ -2,22 +2,7 @@
 
 module WasteCarriersEngine
   class CompanyNameForm < BaseForm
-    attr_accessor :business_type, :company_name
-
-    def initialize(transient_registration)
-      super
-      # We only use this for the correct microcopy
-      self.business_type = @transient_registration.business_type
-      self.company_name = @transient_registration.company_name
-    end
-
-    def submit(params)
-      # Assign the params for validation and pass them to the BaseForm method for updating
-      self.company_name = params[:company_name]
-      attributes = { company_name: company_name }
-
-      super(attributes, params[:reg_identifier])
-    end
+    delegate :business_type, :company_name, to: :transient_registration
 
     validates :company_name, "waste_carriers_engine/company_name": true
   end
