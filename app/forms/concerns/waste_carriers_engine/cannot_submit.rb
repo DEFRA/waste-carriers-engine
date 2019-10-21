f1bde6338c2b9f3d3ec9b6b4e3cbd89e329bf9c4
cintamani <cintamani.puddu@gmail.com>
2019-10-21 14:51:06 +0100

    Add missing file

---
+++ app/forms/concerns/waste_carriers_engine/cannot_submit.rb
@@ -0,0 +1,10 @@
+module WasteCarriersEngine
+  module CannotSubmit
+    extend ActiveSupport::Concern
+
+    included do
+      # Override BaseForm method as users shouldn't be able to submit this form
+      def submit; end
+    end
+  end
+end
