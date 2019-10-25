b63783350f3b004c1c78e693e316c933c041e224
cintamani <cintamani.puddu@gmail.com>
2019-10-25 11:26:51 +0100

    Specs Refactoring - Proposed changes to basic forms
    From: https://eaflood.atlassian.net/browse/RUBY-692
    
    This is a pure unit test implementation of one of our basic forms, specifically of the BusinessTypeForm.
    There are issues connected with the inability to properly unit-test the validations forwarded to the transient registration itself.

--- spec/forms/waste_carriers_engine/business_type_forms_spec.rb
+++ spec/forms/waste_carriers_engine/business_type_forms_spec.rb
@@ -4,26 +4,104 @@ require "rails_helper"
 
 module WasteCarriersEngine
   RSpec.describe BusinessTypeForm, type: :model do
-    describe "#submit" do
-      context "when the form is valid" do
-        let(:business_type_form) { build(:business_type_form, :has_required_data) }
-        let(:valid_params) { { reg_identifier: business_type_form.reg_identifier, business_type: "limitedCompany" } }
+    let(:transient_registration) { double(:transient_registration) }
+    subject(:business_type_form) { described_class.new(transient_registration) }
 
-        it "should submit" do
-          expect(business_type_form.submit(valid_params)).to eq(true)
-        end
+    describe "#initialize" do
+      it "assigns an instance variable called transient_registration" do
+        expect(business_type_form.instance_variable_get(:@transient_registration)).to eq(transient_registration)
+      end
+    end
+
+    context "validations" do
+      let(:validators) { business_type_form._validators }
+
+      it "validates the business type using the BusinessTypeValidator class" do
+        expect(validators.keys).to include(:business_type)
+
+        validator = validators[:business_type].first
+
+        expect(validator.class)
+          .to eq(DefraRuby::Validators::BusinessTypeValidator)
+        expect(validator.options).to include(allow_overseas: true)
       end
 
-      context "when the form is not valid" do
-        let(:business_type_form) { build(:business_type_form, :has_required_data) }
-        let(:invalid_params) { { reg_identifier: "foo", business_type: "foo" } }
+      # TODO: These validations are defined in the BaseForm
+      it "validates the registration identifier using the RegIdentifier class" do
+        expect(validators.keys).to include(:reg_identifier)
+        expect(validators[:reg_identifier].first.class)
+          .to eq(WasteCarriersEngine::RegIdentifierValidator)
+      end
 
-        it "should not submit" do
-          expect(business_type_form.submit(invalid_params)).to eq(false)
+      it "validates the transient_registration" do
+        skip "TODO: Cannot get `validate` defined validators in list of _validators"
+      end
+    end
+
+    context "delegates" do
+      describe "#business_type" do
+        it "delegates the business_type to the transient_registration" do
+          # We use a double here as we want to make sure we return the same object, not just a string
+          # that can come from somewere else and pass the scenario for equality.
+          business_type = double(:business_type)
+
+          expect(transient_registration).to receive(:business_type).and_return(business_type)
+          expect(business_type_form.business_type).to eq(business_type)
         end
       end
     end
 
-    include_examples "validate business_type", :business_type_form
+    describe "#submit" do
+      let(:params) { {} }
+
+      before do
+        expect(business_type_form).to receive(:valid?).and_return(is_form_valid)
+        expect(transient_registration).to receive(:assign_attributes).with(params)
+      end
+
+      context "when the form is valid" do
+        let(:is_form_valid) { true }
+
+        before do
+          # TODO: if we are stubbing the main form `validate`, the transient_registration `validate` never gets called
+          # expect(transient_registration).to receive(:valid?).and_return(is_transient_registration_valid)
+        end
+
+        context "when the transient_registration is valid" do
+          let(:is_transient_registration_valid) { true }
+
+          it "should submit" do
+            expect(transient_registration).to receive(:save!).and_return(true)
+            expect(business_type_form.submit(params)).to eq(true)
+          end
+        end
+
+        context "when the transient_registration is not valid" do
+          let(:is_transient_registration_valid) { false }
+
+          it "should not submit" do
+            skip "TODO: if we are stubbing the main form `validate`, the transient_registration `validate` never gets called"
+
+            error = [0, "my message"]
+            errors = [error]
+            error_base = double(:error_base)
+
+            expect(transient_registration).to receive(:errors).and_return(errors)
+            expect(error).to receive(:[]).with(:base).and_return(error_base)
+            expect(error_base).to receive(:<<).with("my message")
+
+            expect(business_type_form.submit(params)).to eq(false)
+          end
+        end
+      end
+
+      context "when the form is not valid" do
+        let(:is_form_valid) { false }
+
+        it "should not submit" do
+          expect(business_type_form.submit(params)).to eq(false)
+        end
+      end
+    end
   end
 end
