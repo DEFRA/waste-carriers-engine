3d0020a909fb2c7c679c12d26d43ea95abe3ec9d
irisfaraway <iris.faraway@gmail.com>
2019-11-08 18:52:00 +0000

    Create rudimentary PersonMatchService
    This also means we can finally start adding the BaseService pattern!

---
+++ app/services/waste_carriers_engine/convictions_check/person_match_service.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rest-client"
+
+module WasteCarriersEngine
+  module ConvictionsCheck
+    class PersonMatchService < BaseService
+      def run(first_name:, last_name:, date_of_birth:)
+        @first_name = first_name
+        @last_name = last_name
+        @date_of_birth = date_of_birth
+      end
+    end
+  end
+end
---
+++ spec/services/waste_carriers_engine/convictions_check/person_match_service_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteCarriersEngine
+  module ConvictionsCheck
+    RSpec.describe PersonMatchService do
+      describe "run" do
+        let(:first_name) { "foo" }
+        let(:last_name) { "bar" }
+        let(:date_of_birth) { Date.today }
+
+        let(:subject) do
+          described_class.run(first_name: first_name,
+                              last_name: last_name,
+                              date_of_birth: date_of_birth)
+        end
+
+        it "does not explode" do
+          expect { subject }.to_not raise_error
+        end
+      end
+    end
+  end
+end
