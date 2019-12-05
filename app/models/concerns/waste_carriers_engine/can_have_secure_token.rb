595c35de089fed1a497325da85ab98a0f106eefa
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-12-05 15:45:11 +0000

    Switch to using new SecureTokenService
    Use the new service we added which will generate a 24 char just like WEX.

--- app/models/concerns/waste_carriers_engine/can_have_secure_token.rb
+++ app/models/concerns/waste_carriers_engine/can_have_secure_token.rb
@@ -11,7 +11,7 @@ module WasteCarriersEngine
       before_create :generate_unique_secure_token
 
       def generate_unique_secure_token
-        self.token = SecureRandom.base64(24)
+        self.token = SecureTokenService.run
       end
     end
   end
