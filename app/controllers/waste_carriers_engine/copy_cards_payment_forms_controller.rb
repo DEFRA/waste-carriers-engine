ba5395f7554d70e3635f19722295a7f7a101dc7c
cintamani <cintamani.puddu@gmail.com>
2019-12-05 16:07:36 +0000

    Take away changes related to order copy cards refactoring

--- app/controllers/waste_carriers_engine/copy_cards_payment_forms_controller.rb
+++ app/controllers/waste_carriers_engine/copy_cards_payment_forms_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module WasteCarriersEngine
-  class CopyCardsPaymentFormsController < FormsController
-    include OrderCopyCardsPermissionChecks
-
+  class CopyCardsPaymentFormsController < BaseOrderCopyCardsFormsController
     def new
       super(CopyCardsPaymentForm, "copy_cards_payment_form")
     end
