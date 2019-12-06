45edc529f4e245bc4f94ec08cd7a317ef8439276
irisfaraway <iris.faraway@gmail.com>
2019-12-06 13:16:56 +0000

    Raise error if trying to renew with unpaid balance

--- app/services/waste_carriers_engine/registration_completion_service.rb
+++ app/services/waste_carriers_engine/registration_completion_service.rb
@@ -3,8 +3,26 @@
 module WasteCarriersEngine
   class RegistrationCompletionService < BaseService
     def run(registration:)
-      registration.metaData.date_activated = Time.current
-      registration.metaData.activate!
+      @registration = registration
+
+      activate_registration if can_be_completed?
+    end
+
+    private
+
+    def activate_registration
+      @registration.metaData.date_activated = Time.current
+      @registration.metaData.activate!
+    end
+
+    def can_be_completed?
+      raise_unpaid_balance_error if @registration.unpaid_balance?
+
+      true
+    end
+
+    def raise_unpaid_balance_error
+      raise "Registration #{@registration.reg_identifier} cannot be activated due to unpaid balance"
     end
   end
 end
--- spec/services/waste_carriers_engine/registration_completion_service_spec.rb
+++ spec/services/waste_carriers_engine/registration_completion_service_spec.rb
@@ -13,14 +13,28 @@ module WasteCarriersEngine
     describe "run" do
       before { allow(Time).to receive(:current).and_return(current_time) }
 
-      it "updates the date_activated" do
-        registration.metaData.update_attributes(date_activated: nil)
+      context "when there is no unpaid balance" do
+        before { allow(registration).to receive(:unpaid_balance?).and_return(false) }
 
-        expect { service }.to change { registration.metaData.reload.date_activated }.to(current_time)
+        it "updates the date_activated" do
+          registration.metaData.update_attributes(date_activated: nil)
+
+          expect { service }.to change { registration.metaData.reload.date_activated }.to(current_time)
+        end
+
+        it "activates the registration" do
+          expect { service }.to change { registration.active? }.from(false).to(true)
+        end
       end
 
-      it "activates the registration" do
-        expect { service }.to change { registration.active? }.from(false).to(true)
+      context "when the balance is unpaid" do
+        before { allow(registration).to receive(:unpaid_balance?).and_return(true) }
+
+        it "raises an error" do
+          message = "Registration #{registration.reg_identifier} cannot be activated due to unpaid balance"
+
+          expect { service }.to raise_error(RuntimeError, message)
+        end
       end
     end
   end
