ff3fbbffb4a0b2e4edc7e33061a6aa5e6a8bc816
cintamani <cintamani.puddu@gmail.com>
2019-12-12 14:00:17 +0100

    Fx bug on renewal start journey redirect
    Since now the start page of the journey can be hit with either a token or a reg_identifier, we have to guarantee that the behaviour is going to be the same in each case.

--- app/forms/waste_carriers_engine/renewal_start_form.rb
+++ app/forms/waste_carriers_engine/renewal_start_form.rb
@@ -15,6 +15,7 @@ module WasteCarriersEngine
     def find_or_initialize_transient_registration(token)
       # TODO: Downtime at deploy when releasing token?
       @transient_registration = RenewingRegistration.where(token: token).first ||
+                                RenewingRegistration.where(reg_identifier: token).first ||
                                 RenewingRegistration.new(reg_identifier: token)
     end
   end
