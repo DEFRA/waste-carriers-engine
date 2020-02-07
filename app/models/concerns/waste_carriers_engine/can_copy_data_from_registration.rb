bad73c14d7670955f7a6be198c2e43d4dbba9f40
irisfaraway <iris.faraway@gmail.com>
2020-02-07 18:36:18 +0000

    Reduce cognitive complexity

--- app/models/concerns/waste_carriers_engine/can_copy_data_from_registration.rb
+++ app/models/concerns/waste_carriers_engine/can_copy_data_from_registration.rb
@@ -5,21 +5,19 @@ module WasteCarriersEngine
     extend ActiveSupport::Concern
 
     included do
-      after_initialize :copy_data_from_registration, if: :new_record?
+      after_initialize :copy_data_from_registration, if: -> { new_record? && valid? }
     end
 
     def copy_data_from_registration
-      # Don't try to get Registration data with an invalid reg_identifier
-      return unless valid? && new_record?
-
-      # Don't copy object IDs as Mongo should generate new unique ones
-      attributes = registration.attributes.except(*options[:ignorable_attributes])
-
-      assign_attributes(strip_whitespace(attributes))
-
+      copy_attributes_from_registration
       copy_addresses_from_registration if options[:copy_addresses]
       copy_people_from_registration if options[:copy_people]
       remove_invalid_attributes if options[:remove_invalid_attributes]
+    end
+
+    def copy_attributes_from_registration
+      attributes = registration.attributes.except(*options[:ignorable_attributes])
+      assign_attributes(strip_whitespace(attributes))
     end
 
     def copy_addresses_from_registration
