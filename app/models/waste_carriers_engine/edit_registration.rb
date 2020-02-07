53db68432a02badbf46a388ff50ac20ddbfe89b9
irisfaraway <iris.faraway@gmail.com>
2020-02-07 16:50:30 +0000

    Add edit state machine (with one state)

--- app/models/waste_carriers_engine/edit_registration.rb
+++ app/models/waste_carriers_engine/edit_registration.rb
@@ -2,6 +2,8 @@
 
 module WasteCarriersEngine
   class EditRegistration < TransientRegistration
+    include CanUseEditRegistrationWorkflow
+
     validates :reg_identifier, "waste_carriers_engine/reg_identifier": true
   end
 end
--- spec/models/waste_carriers_engine/edit_registration_spec.rb
+++ spec/models/waste_carriers_engine/edit_registration_spec.rb
@@ -6,9 +6,17 @@ module WasteCarriersEngine
   RSpec.describe EditRegistration, type: :model do
     subject(:edit_registration) { build(:edit_registration) }
 
+    context "default status" do
+      context "when an EditRegistration is created" do
+        it "has the state of :edit_form" do
+          expect(edit_registration).to have_state(:edit_form)
+        end
+      end
+    end
+
     context "Validations" do
       describe "reg_identifier" do
-        context "when a EditRegistration is created" do
+        context "when an EditRegistration is created" do
           it "is not valid if the reg_identifier is in the wrong format" do
             edit_registration.reg_identifier = "foo"
             expect(edit_registration).to_not be_valid
