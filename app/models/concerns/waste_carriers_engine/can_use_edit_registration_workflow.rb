9b657d2c3ff71220eecd8e4ca8e05e5e7cef5a45
irisfaraway <iris.faraway@gmail.com>
2020-02-11 12:42:35 +0000

    Add states

--- app/models/concerns/waste_carriers_engine/can_use_edit_registration_workflow.rb
+++ app/models/concerns/waste_carriers_engine/can_use_edit_registration_workflow.rb
@@ -5,6 +5,7 @@ module WasteCarriersEngine
     extend ActiveSupport::Concern
     include Mongoid::Document
 
+    # rubocop:disable Metrics/BlockLength
     included do
       include AASM
 
@@ -14,6 +15,28 @@ module WasteCarriersEngine
         # States / forms
         state :edit_form, initial: true
 
+        # Registration and account
+        state :cbd_type_form
+
+        # Business details
+        state :company_name_form
+        state :main_people_form
+        state :company_postcode_form
+        state :company_address_form
+        state :company_address_manual_form
+
+        # Contact details
+        state :contact_name_form
+        state :contact_phone_form
+        state :contact_email_form
+        state :contact_postcode_form
+        state :contact_address_form
+        state :contact_address_manual_form
+
+        # Location
+        state :location_form
+
+        # Complete an edit
         state :declaration_form
         state :edit_complete_form
 
@@ -31,6 +54,7 @@ module WasteCarriersEngine
                       to: :edit_form
         end
       end
+      # rubocop:enable Metrics/BlockLength
     end
   end
 end
