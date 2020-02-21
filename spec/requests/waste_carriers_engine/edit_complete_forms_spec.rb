4c06b767e789c224429b810bee76a4df22bbde4e
irisfaraway <iris.faraway@gmail.com>
2020-02-21 18:39:02 +0000

    Copy data from EditRegistration to Registration

--- spec/requests/waste_carriers_engine/edit_complete_forms_spec.rb
+++ spec/requests/waste_carriers_engine/edit_complete_forms_spec.rb
@@ -19,16 +19,45 @@ module WasteCarriersEngine
         end
 
         context "when a valid edit registration exists" do
+          let(:updated_email) { "updated@example.com" }
+          let(:updated_registered_address) { build(:address, :registered, postcode: "UP1 2DT") }
+          let(:updated_contact_address) { build(:address, :contact, postcode: "D1 1FF") }
+          let(:updated_person) { build(:key_person, :main, first_name: "Updated") }
+
           let(:transient_registration) do
             create(:edit_registration,
+                   contact_email: updated_email,
+                   addresses: [updated_registered_address, updated_contact_address],
+                   key_people: [updated_person],
                    workflow_state: "edit_complete_form")
           end
+          let(:registration) { transient_registration.registration }
 
           context "when the workflow_state is correct" do
-            it "deletes the transient object" do
-              get new_edit_complete_form_path(transient_registration.token)
+            it "updates the registration with the new data and deletes the transient object" do
+              old_finance_details = registration.finance_details
 
+              get new_edit_complete_form_path(transient_registration.token)
+              registration.reload
+
+              # Update base attributes
+              expect(registration.contact_email).to eq(updated_email)
+
+              # Update key people
+              expect(registration.main_people.count).to eq(1)
+              expect(registration.main_people.first.first_name).to eq(updated_person.first_name)
+
+              # Update addresses
+              expect(registration.addresses.count).to eq(2)
+              expect(registration.registered_address.postcode).to eq(updated_registered_address.postcode)
+              expect(registration.contact_address.postcode).to eq(updated_contact_address.postcode)
+
+              # But don't modify finance details
+              expect(registration.finance_details).to eq(old_finance_details)
+
+              # Delete the transient registration
               expect(WasteCarriersEngine::TransientRegistration.count).to eq(0)
+
               expect(response).to have_http_status(200)
             end
           end
@@ -38,8 +67,19 @@ module WasteCarriersEngine
               transient_registration.update_attributes(workflow_state: "declaration_form")
             end
 
-            it "redirects to the correct page" do
+            it "redirects to the correct page, does not update the registration and does not delete the transient object" do
               get new_edit_complete_form_path(transient_registration.token)
+              registration.reload
+
+              expect(registration.contact_email).to_not eq(updated_email)
+
+              expect(registration.main_people.first.first_name).to_not eq(updated_person.first_name)
+
+              expect(registration.registered_address.postcode).to_not eq(updated_registered_address.postcode)
+              expect(registration.contact_address.postcode).to_not eq(updated_contact_address.postcode)
+
+              expect(WasteCarriersEngine::TransientRegistration.count).to eq(1)
+
               expect(response).to redirect_to(new_declaration_form_path(transient_registration.token))
             end
           end
