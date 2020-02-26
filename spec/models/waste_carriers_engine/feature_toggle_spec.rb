9b1058cea6524538f64302dbc8f5aa73c64fcfe8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-02-26 18:36:32 +0000

    Final changes to match WEX latest

--- spec/models/waste_carriers_engine/feature_toggle_spec.rb
+++ spec/models/waste_carriers_engine/feature_toggle_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-module WasteCarrierssEngine
+module WasteCarriersEngine
   RSpec.describe FeatureToggle do
     describe ".active?" do
       context "when a feature toggle config exist" do
@@ -42,7 +42,7 @@ module WasteCarrierssEngine
 
         context "when the feature toggle is an environment variable" do
           it "returns true" do
-            ENV["env_variable_test_feature"] = "true"
+            ENV["ENV_VARIABLE_TEST_FEATURE"] = "true"
 
             expect(described_class.active?("env_variable_test_feature")).to be_truthy
           end
