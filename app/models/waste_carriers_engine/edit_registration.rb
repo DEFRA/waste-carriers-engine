6bace1daa4205e6538e9783fe0f43e924f50970a
irisfaraway <iris.faraway@gmail.com>
2020-02-28 17:37:05 +0000

    Create order for the CBD type change during edit
    The description for this still needs fixing - it says 'renewal'.

--- app/models/waste_carriers_engine/edit_registration.rb
+++ app/models/waste_carriers_engine/edit_registration.rb
@@ -26,5 +26,13 @@ module WasteCarriersEngine
     def registration
       @_registration ||= Registration.find_by(reg_identifier: reg_identifier)
     end
+
+    def prepare_for_payment(mode, user)
+      EditFinanceDetailsBuilderService.run(
+        user: user,
+        transient_registration: self,
+        payment_method: mode
+      )
+    end
   end
 end
