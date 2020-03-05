94c66b8cb36b36abb381017e13c6a7b373d7d0bd
cintamani <cintamani.puddu@gmail.com>
2020-03-05 15:38:01 +0000

    fix rubocop

--- spec/models/waste_carriers_engine/new_registration_workflow/contact_postcode_form_spec.rb
+++ spec/models/waste_carriers_engine/new_registration_workflow/contact_postcode_form_spec.rb
@@ -5,8 +5,8 @@ require "rails_helper"
 module WasteCarriersEngine
   RSpec.describe NewRegistration do
     it_behaves_like "a postcode transition",
-                      previous_state: :contact_email_form,
-                      address_type: "contact",
-                      factory: :new_registration
+                    previous_state: :contact_email_form,
+                    address_type: "contact",
+                    factory: :new_registration
   end
 end
