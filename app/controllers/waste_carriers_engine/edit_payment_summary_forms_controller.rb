cb1fdd6731f4d153b84d276ff87fb0ce6889441d
irisfaraway <iris.faraway@gmail.com>
2020-03-09 15:21:42 +0000

    Add shared partials to display order details
    These make use of our new OrderAndTotalPresenter.

--- app/controllers/waste_carriers_engine/edit_payment_summary_forms_controller.rb
+++ app/controllers/waste_carriers_engine/edit_payment_summary_forms_controller.rb
@@ -15,5 +15,9 @@ module WasteCarriersEngine
     def transient_registration_attributes
       params.fetch(:edit_payment_summary_form, {}).permit(:temp_payment_method)
     end
+
+    def fetch_presenters
+      @order_and_total_presenter = OrderAndTotalPresenter.new(@edit_payment_summary_form, view_context)
+    end
   end
 end
