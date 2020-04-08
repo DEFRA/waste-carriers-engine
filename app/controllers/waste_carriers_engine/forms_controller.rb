767a103d24f3460480379e2855fa159c96a51c97
cintamani <cintamani.puddu@gmail.com>
2020-04-08 14:46:36 +0100

    Move new method to presenter rather than helper

--- app/controllers/waste_carriers_engine/forms_controller.rb
+++ app/controllers/waste_carriers_engine/forms_controller.rb
@@ -4,8 +4,6 @@ module WasteCarriersEngine
   class FormsController < ApplicationController
     include ActionView::Helpers::UrlHelper
 
-    helper FormsHelper
-
     before_action :back_button_cache_buster
     before_action :validate_token
 
--- spec/presenters/waste_carriers_engine/check_your_answers_form_presenter_spec.rb
+++ spec/presenters/waste_carriers_engine/check_your_answers_form_presenter_spec.rb
@@ -3,35 +3,43 @@
 require "rails_helper"
 
 module WasteCarriersEngine
-  RSpec.describe WasteCarriersEngine::FormsHelper, type: :helper do
+  RSpec.describe CheckYourAnswersFormPresenter do
+    subject { described_class.new(transient_registration) }
+
     describe "#show_smart_answers_results?" do
       context "when the transient_registration is a charity" do
         let(:transient_registration) { double(:transient_registration, charity?: true) }
 
         it "returns false" do
-          expect(helper.show_smart_answers_results?(transient_registration)).to eq(false)
+          expect(subject.show_smart_answers_results?).to eq(false)
         end
       end
 
       context "when the transient_registration is not a charity" do
+        before do
+          expect(subject).to receive(:new_registration?).and_return(new_registration)
+        end
+
         context "when the transient_registration is not a new_registration" do
-          let(:transient_registration) { double(:transient_registration, charity?: false, new_registration?: false) }
+          let(:new_registration) { false }
+          let(:transient_registration) { double(:transient_registration, charity?: false) }
 
           it "returns true" do
-            expect(helper.show_smart_answers_results?(transient_registration)).to eq(true)
+            expect(subject.show_smart_answers_results?).to eq(true)
           end
         end
 
         context "when the transient_registration is a new_registration" do
+          let(:new_registration) { true }
           let(:transient_registration) do
-            double(:transient_registration, charity?: false, new_registration?: true, tier_known?: tier_known)
+            double(:transient_registration, charity?: false, tier_known?: tier_known)
           end
 
           context "when the tier is known to the user" do
             let(:tier_known) { true }
 
             it "returns false" do
-              expect(helper.show_smart_answers_results?(transient_registration)).to eq(false)
+              expect(subject.show_smart_answers_results?).to eq(false)
             end
           end
 
@@ -39,7 +47,7 @@ module WasteCarriersEngine
             let(:tier_known) { false }
 
             it "returns true" do
-              expect(helper.show_smart_answers_results?(transient_registration)).to eq(true)
+              expect(subject.show_smart_answers_results?).to eq(true)
             end
           end
         end
