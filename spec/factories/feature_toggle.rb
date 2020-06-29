43e616e40c2a768042cb008df92b82753fad6b80
cintamani <cintamani.puddu@gmail.com>
2020-06-29 13:32:10 +0100

    Specify correct namespace for factory

--- spec/factories/feature_toggle.rb
+++ spec/factories/feature_toggle.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :feature_toggle do
+  factory :feature_toggle, class: WasteCarriersEngine::FeatureToggle do
     key { "test-feature" }
 
     active { false }
