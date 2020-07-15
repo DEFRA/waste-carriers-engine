788ee4e926e474cfb61cb89da92a7bfe04123998
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-15 23:52:06 +0100

    Update existing tests to match changed behaviour
    Try to cover the fact recipt_email is now a field, but that we only care about it if the payment type is card.

--- spec/forms/waste_carriers_engine/payment_summary_forms_spec.rb
+++ spec/forms/waste_carriers_engine/payment_summary_forms_spec.rb
@@ -10,7 +10,8 @@ module WasteCarriersEngine
         let(:valid_params) do
           {
             token: payment_summary_form.token,
-            temp_payment_method: payment_summary_form.temp_payment_method
+            temp_payment_method: payment_summary_form.temp_payment_method,
+            receipt_email: "foo@example.com"
           }
         end
 
@@ -21,7 +22,13 @@ module WasteCarriersEngine
 
       context "when the form is not valid" do
         let(:payment_summary_form) { build(:payment_summary_form, :has_required_data) }
-        let(:invalid_params) { { temp_payment_method: "foo" } }
+        let(:invalid_params) do
+          {
+            token: payment_summary_form.token,
+            temp_payment_method: "foo",
+            receipt_email: "foo@com"
+          }
+        end
 
         it "should not submit" do
           expect(payment_summary_form.submit(invalid_params)).to eq(false)
@@ -43,6 +50,28 @@ module WasteCarriersEngine
           it "is valid" do
             expect(payment_summary_form).to be_valid
           end
+
+          context "but the receipt email has been set" do
+            before do
+              payment_summary_form.transient_registration.receipt_email = receipt_email
+            end
+
+            context "to something invalid" do
+              let(:receipt_email) { "foo@bar" }
+
+              it "is still valid" do
+                expect(payment_summary_form).to be_valid
+              end
+            end
+
+            context "to nothing" do
+              let(:receipt_email) { "" }
+
+              it "is still valid" do
+                expect(payment_summary_form).to be_valid
+              end
+            end
+          end
         end
 
         context "when a temp_payment_method is card" do
@@ -50,6 +79,28 @@ module WasteCarriersEngine
 
           it "is valid" do
             expect(payment_summary_form).to be_valid
+          end
+
+          context "and the receipt email has been set" do
+            before do
+              payment_summary_form.transient_registration.receipt_email = receipt_email
+            end
+
+            context "to something invalid" do
+              let(:receipt_email) { "foo@bar" }
+
+              it "is is not valid" do
+                expect(payment_summary_form).not_to be_valid
+              end
+            end
+
+            context "to nothing" do
+              let(:receipt_email) { "" }
+
+              it "is is not valid" do
+                expect(payment_summary_form).not_to be_valid
+              end
+            end
           end
         end
 
