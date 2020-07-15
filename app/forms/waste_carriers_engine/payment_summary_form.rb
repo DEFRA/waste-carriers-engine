302b93acf2d0dbfed0c17ec27b7e6ec0048542e3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-15 22:45:58 +0100

    Refactor solution and add validation
    Following the pattern that seems to be more recently used we delegate receipt_email to the transient_registration rather than creating our own attribute.
    
    This means we can hook in the property getting saved and validated. We have also added `paying_by_card?` to handle the scenario of only validating the email if payment by card is selected.

--- app/forms/waste_carriers_engine/payment_summary_form.rb
+++ app/forms/waste_carriers_engine/payment_summary_form.rb
@@ -3,10 +3,12 @@
 module WasteCarriersEngine
   class PaymentSummaryForm < ::WasteCarriersEngine::BaseForm
     delegate :temp_payment_method, to: :transient_registration
+    delegate :receipt_email, to: :transient_registration, allow_nil: true
 
-    attr_accessor :type_change, :registration_cards, :registration_card_charge, :total_charge, :receipt_email
+    attr_accessor :type_change, :registration_cards, :registration_card_charge, :total_charge
 
     validates :temp_payment_method, inclusion: { in: %w[card bank_transfer] }
+    validates :receipt_email, "defra_ruby/validators/email": true, if: :paying_by_card?
 
     def self.can_navigate_flexibly?
       false
@@ -18,7 +20,15 @@ module WasteCarriersEngine
       self.registration_cards = transient_registration.temp_cards || 0
       self.registration_card_charge = transient_registration.total_registration_card_charge
       self.total_charge = transient_registration.total_to_pay
-      self.receipt_email = transient_registration.email_to_send_receipt
+    end
+
+    private
+
+    # TODO: This method is duplicated in
+    # app/models/concerns/waste_carriers_engine/can_use_new_registration_workflow.rb.
+    # Consider refactoring to being on the transient_registration itself
+    def paying_by_card?
+      temp_payment_method == "card"
     end
   end
 end
