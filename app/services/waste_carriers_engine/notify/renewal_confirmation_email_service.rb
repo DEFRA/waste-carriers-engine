ec3e979c0dd554288c88f522dd0f9deb17749591
Toby Privett <toby@snaplab.co.uk>
2021-04-30 14:55:41 +0100

    Introduce RenewalConfirmationEmailService to send emails with notify

---
+++ app/services/waste_carriers_engine/notify/renewal_confirmation_email_service.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module WasteCarriersEngine
+  module Notify
+    class RenewalConfirmationEmailService < BaseSendEmailService
+      private
+
+      def notify_options
+        {
+          email_address: @registration.contact_email,
+          template_id: "6d54a9bc-9b62-4d93-a40a-d06d04ed58ca",
+          personalisation: {
+            reg_identifier: @registration.reg_identifier,
+            registration_type: registration_type,
+            first_name: @registration.first_name,
+            last_name: @registration.last_name,
+            phone_number: @registration.phone_number,
+            registered_address: registered_address,
+            date_activated: date_activated
+          }
+        }
+      end
+
+      def registered_address
+        certificate_presenter.registered_address_fields.join("\r\n")
+      end
+
+      def date_activated
+        @registration.metaData.date_activated.in_time_zone("London").to_date.to_s
+      end
+
+      def certificate_presenter
+        @_certificate_presenter ||= CertificatePresenter.new(@registration)
+      end
+    end
+  end
+end
