02ff74a8892b24ae5e485f7a0018a006946fbbd7
Rebecca <Beckyrose200@aol.com>
2022-01-13 12:27:58 +0000

    Robocop white space deleted

--- spec/requests/waste_carriers_engine/cannot_renew_company_no_change_forms_spec.rb
+++ spec/requests/waste_carriers_engine/cannot_renew_company_no_change_forms_spec.rb
@@ -19,7 +19,7 @@ module WasteCarriersEngine
                    :has_required_data,
                    account_email: user.email,
                    workflow_state: "cannot_renew_company_no_change_form")
-          end
+        end
          
           context "When the form is loaded" do
             it "Has correct title" do
