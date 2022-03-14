d160971b91f0c6cd47064458dd8722dbfef1c996
Rebecca Ransome <beckyrose200@aol.com>
2022-03-14 11:21:18 +0000

    Companies name and address populating on page

--- app/controllers/waste_carriers_engine/check_registered_company_name_forms_controller.rb
+++ app/controllers/waste_carriers_engine/check_registered_company_name_forms_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require "defra_ruby_companies_house"
+
 module WasteCarriersEngine
   class CheckRegisteredCompanyNameFormsController < ::WasteCarriersEngine::FormsController
     def new
       super(CheckRegisteredCompanyNameForm, "check_registered_company_name_form")
+      @companies_house_check = DefraRubyCompaniesHouse.new(@transient_registration.company_no)
     end
 
     def create
