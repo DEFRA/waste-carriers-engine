05b49b050cf55dd257aa17af2a53555d9c757b8c
Rebecca Ransome <beckyrose200@aol.com>
2022-03-16 11:40:11 +0000

    Changed factory bot from renewing registration to new registration for check_registered_company_name_form

--- spec/factories/forms/check_registered_company_name_form.rb
+++ spec/factories/forms/check_registered_company_name_form.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :check_registered_company_name_form, class: WasteCarriersEngine::CheckRegisteredCompanyNameForm do
     trait :has_required_data do
-      initialize_with { new(create(:renewing_registration, :has_required_data, workflow_state: "check_registered_company_name_form")) }
+      initialize_with { new(create(:new_registration, :has_required_data, workflow_state: "check_registered_company_name_form")) }
     end
   end
 end
