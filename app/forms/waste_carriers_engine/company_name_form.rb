76bf80c76adfcf23b38387fcfb7b2bb48cbef6d4
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-03-25 16:26:52 +0000

    Suppress any existing company name on a renewing registration to enforce entering a new one

--- app/forms/waste_carriers_engine/company_name_form.rb
+++ app/forms/waste_carriers_engine/company_name_form.rb
@@ -5,5 +5,14 @@ module WasteCarriersEngine
     delegate :business_type, :company_name, :registered_company_name, :tier, to: :transient_registration
 
     validates :company_name, "waste_carriers_engine/company_name": true
+
+    def initialize(transient_registration)
+      # Any existing company name should not be used for a registration renewal.
+      if transient_registration.is_a?(WasteCarriersEngine::RenewingRegistration)
+        transient_registration.company_name = nil
+      end
+
+      super(transient_registration)
+    end
   end
 end
