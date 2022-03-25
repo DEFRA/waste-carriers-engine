7fe6975dffd3fac91dc958b547484437e14c78f2
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-03-25 18:59:35 +0000

    Add registration company_name_required? method and refactor accordingly

--- app/forms/waste_carriers_engine/company_name_form.rb
+++ app/forms/waste_carriers_engine/company_name_form.rb
@@ -2,13 +2,15 @@
 
 module WasteCarriersEngine
   class CompanyNameForm < ::WasteCarriersEngine::BaseForm
-    delegate :business_type, :company_name, :registered_company_name, :tier, to: :transient_registration
+    delegate :business_type, :company_name, :company_name_required?,
+             :registered_company_name, :tier, to: :transient_registration
 
     validates :company_name, "waste_carriers_engine/company_name": true
 
     def initialize(transient_registration)
-      # Any existing company name should not be used for a registration renewal.
-      if transient_registration.is_a?(WasteCarriersEngine::RenewingRegistration)
+      # Any existing company name should not be used for a registration renewal where company_name is optional.
+      if transient_registration.is_a?(WasteCarriersEngine::RenewingRegistration) &&
+         !transient_registration.company_name_required?
         transient_registration.company_name = nil
       end
 
