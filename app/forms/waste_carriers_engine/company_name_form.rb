dca64736e60800954a4c1b02051b0b593b88d2a3
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-03-28 10:56:55 +0100

    Move the setting of the company name to nil from company_name_form to renewal_start_forms_controller

--- app/forms/waste_carriers_engine/company_name_form.rb
+++ app/forms/waste_carriers_engine/company_name_form.rb
@@ -6,15 +6,5 @@ module WasteCarriersEngine
              :registered_company_name, :tier, to: :transient_registration
 
     validates :company_name, "waste_carriers_engine/company_name": true
-
-    def initialize(transient_registration)
-      # Any existing company name should not be used for a registration renewal where company_name is optional.
-      if transient_registration.is_a?(WasteCarriersEngine::RenewingRegistration) &&
-         !transient_registration.company_name_required?
-        transient_registration.company_name = nil
-      end
-
-      super(transient_registration)
-    end
   end
 end
