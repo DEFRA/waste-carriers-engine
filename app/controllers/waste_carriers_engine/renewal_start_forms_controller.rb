aa0c643415119e49a33d89a6c90a7b369301d5ca
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-03-31 11:27:46 +0100

    Remove unnecessary memoisation from renewal_start_forms_controller

--- app/controllers/waste_carriers_engine/renewal_start_forms_controller.rb
+++ app/controllers/waste_carriers_engine/renewal_start_forms_controller.rb
@@ -14,13 +14,11 @@ module WasteCarriersEngine
 
     private
 
-    # rubocop:disable Naming/MemoizedInstanceVariableName
     def find_or_initialize_transient_registration(token)
-      @transient_registration ||= RenewingRegistration.where(token: token).first ||
-                                  RenewingRegistration.where(reg_identifier: token).first ||
-                                  RenewingRegistration.new(reg_identifier: token)
+      @transient_registration = RenewingRegistration.where(token: token).first ||
+                                RenewingRegistration.where(reg_identifier: token).first ||
+                                RenewingRegistration.new(reg_identifier: token)
     end
-    # rubocop:enable Naming/MemoizedInstanceVariableName
 
     def should_authenticate_user?
       find_or_initialize_transient_registration(params[:token])
