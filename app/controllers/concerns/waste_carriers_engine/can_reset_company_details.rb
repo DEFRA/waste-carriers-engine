d7ca41af5c05c0b9c9fa892c6b646474a435583c
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-05-03 17:08:16 +0100

    Do not reset company details for renewals

--- app/controllers/concerns/waste_carriers_engine/can_reset_company_details.rb
+++ app/controllers/concerns/waste_carriers_engine/can_reset_company_details.rb
@@ -9,6 +9,9 @@ module WasteCarriersEngine
       # Clear any previous business-type specific attributes to handle cases where the user
       # starts with one business type and then navigates back and changes the business type.
       def reset_company_attributes
+        # The renewals flow has its own logic to handle such changes
+        return unless @transient_registration.is_a?(NewRegistration)
+
         @transient_registration.company_no = nil
         @transient_registration.registered_company_name = nil
         @transient_registration.temp_use_registered_company_details = nil
