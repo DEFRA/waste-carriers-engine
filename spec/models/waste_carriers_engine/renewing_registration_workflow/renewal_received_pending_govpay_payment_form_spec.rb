736c1f4848eebbb30e3ea8ad49baafb893a27220
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-06-15 16:14:23 +0100

    Unit tests for govpay forms workflow

--- spec/models/waste_carriers_engine/renewing_registration_workflow/renewal_received_pending_govpay_payment_form_spec.rb
+++ spec/models/waste_carriers_engine/renewing_registration_workflow/renewal_received_pending_govpay_payment_form_spec.rb
@@ -3,10 +3,10 @@
 require "rails_helper"
 
 module WasteCarriersEngine
-  RSpec.describe RenewalReceivedPendingPaymentForm, type: :model do
+  RSpec.describe RenewalReceivedPendingGovpayPaymentForm, type: :model do
     describe "#workflow_state" do
       it_behaves_like "a fixed final state",
-                      current_state: :renewal_received_pending_payment_form,
+                      current_state: :renewal_received_pending_govpay_payment_form,
                       factory: :renewing_registration
     end
   end
