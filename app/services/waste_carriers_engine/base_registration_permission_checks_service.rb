3ad5558c5ec8cb8323557bd8162e9883e81ff15f
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-07-27 13:22:50 +0100

    Add debugging for intermittent production nil user error

--- app/services/waste_carriers_engine/base_registration_permission_checks_service.rb
+++ app/services/waste_carriers_engine/base_registration_permission_checks_service.rb
@@ -19,6 +19,26 @@ module WasteCarriersEngine
     private
 
     def can?(action, object)
+      unless user.present?
+        e = StandardError.new(
+          "Permissions check requested for nil user, "\
+          "action: #{action}, "\
+          "reg_identifier #{@transient_registration.reg_identifier}, "\
+          "from_magic_link: #{@transient_registration.from_magic_link ? 'true' : 'false'}, "\
+          "workflow_state: #{@transient_registration.workflow_state}, "\
+          "workflow_history: #{@transient_registration.workflow_history}, "\
+          "expires_on: #{@transient_registration.expires_on}, "\
+          "renew_token: #{@transient_registration.registration.renew_token}, "\
+          "metaData.route: #{@transient_registration.metaData.route}, "\
+          "transient_registration created_at: #{@transient_registration.created_at}, "\
+        )
+
+        Airbrake.notify(e, reg_identifier: @transient_registration.reg_identifier) if defined?(Airbrake)
+        Rails.logger.warn e
+        # TODO: Continue so the original error is visible in the logs along with the debug info.
+        # TODO: Revisit this and remove the additional debugging pending further investigation.
+      end
+
       ability = Ability.new(user)
 
       ability.can?(action, object)
