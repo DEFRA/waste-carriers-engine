0dac638d962dfddce6cf0ce66f30fa163fb30839
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-09-15 16:40:52 +0100

    rebase govpay to main

--- spec/models/waste_carriers_engine/renewing_registration_workflow/govpay_form_spec.rb
+++ spec/models/waste_carriers_engine/renewing_registration_workflow/govpay_form_spec.rb
@@ -10,18 +10,18 @@ module WasteCarriersEngine
             :has_conviction_search_result,
             :has_key_people,
             :has_paid_balance,
-            workflow_state: "worldpay_form")
+            workflow_state: "govpay_form")
     end
 
     describe "#workflow_state" do
-      context ":worldpay_form state transitions" do
+      context ":govpay_form state transitions" do
         context "on next" do
           context "when a conviction check is not required" do
             before do
               allow(subject).to receive(:conviction_check_required?).and_return(false)
             end
 
-            context "when there is no pending WorldPay payment" do
+            context "when there is no pending Govpay payment" do
               before do
                 allow(subject).to receive(:pending_online_payment?).and_return(false)
               end
@@ -29,22 +29,18 @@ module WasteCarriersEngine
               include_examples "has next transition", next_state: "renewal_complete_form"
 
               it "does not send a confirmation email after the 'next' event" do
-                # An older incarnation of this spec checked that ActionMailer
-                # did not send *any* emails.
-                # Here we check to see if the notification service has been invoked,
-                # which should tell us if *any* emails have been sent.
                 expect(Notifications::Client).not_to receive(:new)
 
                 subject.next!
               end
             end
 
-            context "when there is a pending WorldPay payment" do
+            context "when there is a pending Govpay payment" do
               before do
                 allow(subject).to receive(:pending_online_payment?).and_return(true)
               end
 
-              include_examples "has next transition", next_state: "renewal_received_pending_worldpay_payment_form"
+              include_examples "has next transition", next_state: "renewal_received_pending_govpay_payment_form"
 
               it "sends a confirmation email after the 'next' event" do
                 expect(Notify::RenewalPendingOnlinePaymentEmailService)
