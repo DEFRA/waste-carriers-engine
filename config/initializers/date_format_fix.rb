006143324262d3d119ac8cfe02b14e639b11b732
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-09-20 09:21:03 +0100

    Rubocop fix

--- config/initializers/date_format_fix.rb
+++ config/initializers/date_format_fix.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require "date"
 
 class Date
   def to_s(format = :default)
-    if formatter = DATE_FORMATS[format]
+    if (formatter = DATE_FORMATS[format])
       if formatter.respond_to?(:call)
         formatter.call(self).to_s
       else
