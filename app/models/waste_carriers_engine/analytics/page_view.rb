480bc9a96510f4aaaf88b559fa572504d8e69d26
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-01-18 19:43:04 +0000

    Temporarily reinstate page_view user_journey_id to support data migration

--- app/models/waste_carriers_engine/analytics/page_view.rb
+++ app/models/waste_carriers_engine/analytics/page_view.rb
@@ -9,6 +9,10 @@ module WasteCarriersEngine
 
       embedded_in :user_journey
 
+      # TODO: Needed temporarily to support the embed_page_views_in_user_journey migration task;
+      # TODO: Remove this after the migration task has been run in production.
+      field :user_journey_id, type: BSON::ObjectId
+
       field :page, type: String
       field :time, type: DateTime
       field :route, type: String
