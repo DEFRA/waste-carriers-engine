54ec6440b8dc1bdb65c4b4279047e6d5296b63e2
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-01-25 17:14:01 +0000

    [RUBY-2845] Refactor certificate generation and link sharing for registrations and renewals, and ensure view certificate token generation.

--- app/services/waste_carriers_engine/notify/certificate_renewal_email_service.rb
+++ app/services/waste_carriers_engine/notify/certificate_renewal_email_service.rb
@@ -5,7 +5,7 @@ module WasteCarriersEngine
     class CertificateRenewalEmailService < BaseSendEmailService
       include WasteCarriersEngine::ApplicationHelper
       include WasteCarriersEngine::CanRecordCommunication
-      include Rails.application.routes.url_helpers
+      include WasteCarriersEngine::CanAttachCertificate
 
       TEMPLATE_ID = "2eae1dbd-08c1-4602-a4d2-e4481a1acc97"
       COMMS_LABEL = "Resend certificate link"
@@ -24,7 +24,7 @@ module WasteCarriersEngine
             registered_address: registered_address,
             date_registered: date_registered,
             phone_number: @registration.phone_number,
-            link_to_file: link_to_file
+            link_to_file: link_to_certificate
           }
         }
       end
