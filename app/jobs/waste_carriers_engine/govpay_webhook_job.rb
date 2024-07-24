7150f136b4bdf91f42ff25258fb350aea9b75cd6
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-07-24 09:47:47 +0100

    Update webhook active_job to handle payments and refunds

--- app/jobs/waste_carriers_engine/govpay_webhook_job.rb
+++ app/jobs/waste_carriers_engine/govpay_webhook_job.rb
@@ -3,7 +3,13 @@
 module WasteCarriersEngine
   class GovpayWebhookJob < ApplicationJob
     def perform(webhook_body)
-      WasteCarriersEngine::GovpayWebhookPaymentService.run(webhook_body)
+      if webhook_body["resource_type"] == "payment"
+        WasteCarriersEngine::GovpayWebhookPaymentService.run(webhook_body)
+      elsif webhook_body["refund_id"].present?
+        WasteCarriersEngine::GovpayWebhookRefundService.run(webhook_body)
+      else
+        raise ArgumentError, "Unrecognised Govpay webhook type: #{webhhook_body}"
+      end
     rescue StandardError => e
       Rails.logger.error "Error running GovpayWebhookJob: #{e}"
       Airbrake.notify(e, webhook_body:)
