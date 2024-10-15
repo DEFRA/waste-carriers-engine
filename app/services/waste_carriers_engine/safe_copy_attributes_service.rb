45f73db84b91f06d3ebc82144c1400d014016395
Jerome Pratt <jerome.pratt@hey.com>
2024-10-15 16:47:20 +0100

    [RUBY-3387] Fix duplicate storage of govpay status by refactoring `SafeCopyAttributesService`
    - Implement `safe_copy_attributes` method to handle attribute copying more efficiently.
    - Introduce private helper methods `get_attributes`, `filter_attributes`, and `find_embedded_relation` to modularize logic.
    - Update specs to cover new functionality, including handling of Hash and BSON::Document sources.
    - Ensure only valid attributes are copied, and embedded documents are processed correctly.

--- app/services/waste_carriers_engine/safe_copy_attributes_service.rb
+++ app/services/waste_carriers_engine/safe_copy_attributes_service.rb
@@ -2,7 +2,6 @@
 
 module WasteCarriersEngine
   class SafeCopyAttributesService < BaseService
-
     attr_accessor :source_instance, :target_class, :embedded_documents, :attributes_to_exclude
 
     def run(source_instance:, target_class:, embedded_documents: [], attributes_to_exclude: [])
@@ -10,23 +9,67 @@ module WasteCarriersEngine
       @target_class = target_class
       @embedded_documents = embedded_documents
       @attributes_to_exclude = attributes_to_exclude
-
-      source_attributes.except(*unsupported_attribute_keys)
+      safe_copy_attributes(source_instance, target_class)
     end
 
-    def source_attributes
-      attributes = source_instance.is_a?(BSON::Document) ? source_instance : source_instance.attributes
+    private
 
-      attributes.except(*attributes_to_exclude)
+    def safe_copy_attributes(source, target_class)
+      attributes = get_attributes(source)
+      filtered_attributes = filter_attributes(attributes, target_class)
+
+      embedded_documents.each do |doc_name|
+        camel_doc_name = doc_name.camelize(:lower)
+        snake_doc_name = doc_name.underscore
+
+        if attributes.key?(camel_doc_name) || attributes.key?(snake_doc_name)
+          embedded_relation = find_embedded_relation(target_class, doc_name)
+          next unless embedded_relation
+
+          embedded_class = embedded_relation.class_name.constantize
+          embedded_data = attributes[camel_doc_name] || attributes[snake_doc_name]
+
+          if embedded_data.is_a?(Array)
+            filtered_attributes[embedded_relation.name.to_s] = embedded_data.map do |embedded_doc|
+              safe_copy_attributes(embedded_doc, embedded_class)
+            end
+          elsif embedded_data.is_a?(Hash)
+            filtered_attributes[embedded_relation.name.to_s] = safe_copy_attributes(embedded_data, embedded_class)
+          end
+        end
+      end
+
+      filtered_attributes
     end
 
-    def target_fields
-      # Include both camelCase (DB) and snake_case (model) attribute names:
-      (target_class.fields.keys + target_class.fields.keys.map(&:underscore)).uniq
+    def find_embedded_relation(target_class, doc_name)
+      target_class.embedded_relations.values.find do |relation|
+        relation.name.to_s == doc_name.underscore || relation.store_as == doc_name
+      end
     end
 
-    def unsupported_attribute_keys
-      source_attributes.except(*target_fields).excluding(embedded_documents).keys
+    def get_attributes(source)
+      if source.is_a?(Hash) || source.is_a?(BSON::Document)
+        source.to_h
+      elsif source.respond_to?(:attributes)
+        source.attributes
+      else
+        raise ArgumentError, "Unsupported source type: #{source.class}"
+      end
+    end
+
+    def filter_attributes(attributes, klass)
+      valid_fields = target_fields(klass)
+      attributes.select { |k, _| valid_fields.include?(k) || valid_fields.include?(k.to_s) }
+               .except(*attributes_to_exclude)
+    end
+
+    def target_fields(klass)
+      @target_fields ||= {}
+      @target_fields[klass] ||= begin
+        fields = klass.fields.keys
+        (fields + fields.map(&:underscore) + fields.map { |f| f.camelize(:lower) }).uniq
+      end
     end
   end
 end
--- spec/services/waste_carriers_engine/safe_copy_attributes_service_spec.rb
+++ spec/services/waste_carriers_engine/safe_copy_attributes_service_spec.rb
@@ -5,26 +5,21 @@ require "rails_helper"
 module WasteCarriersEngine
   RSpec.describe SafeCopyAttributesService do
     describe "#run" do
-
       subject(:run_service) { described_class.run(source_instance:, target_class:, embedded_documents:, attributes_to_exclude:) }
 
-      let(:embedded_documents) { nil }
-      let(:exclusion_list) { nil }
-      let(:attributes_to_exclude) { exclusion_list }
+      let(:embedded_documents) { [] }
+      let(:attributes_to_exclude) { [] }
 
-      # ensure all available attributes are populated on the source
       before do
-        unless source_instance.is_a?(BSON::Document)
+        unless source_instance.is_a?(BSON::Document) || source_instance.is_a?(Hash)
           source_instance.class.fields.keys.excluding("_id").each do |attr|
             next unless source_instance.send(attr).blank? && source_instance.respond_to?("#{attr}=")
-
             source_instance.send "#{attr}=", 0
           end
         end
       end
 
       shared_examples "returns the correct attributes" do
-
         it { expect { run_service }.not_to raise_error }
 
         it "returns copyable attributes" do
@@ -40,42 +35,88 @@ module WasteCarriersEngine
         context "with an exclusion list" do
           let(:attributes_to_exclude) { exclusion_list }
 
-          it "does not return the excluded attibutes" do
-            expect(run_service.keys).not_to include(exclusion_list)
+          it "does not return the excluded attributes" do
+            expect(run_service.keys).not_to include(*exclusion_list)
           end
         end
       end
 
       context "when the target is a Registration" do
         let(:target_class) { Registration }
-        # include all embeds_many relationships
         let(:embedded_documents) { %w[addresses financeDetails metaData] }
         let(:copyable_attributes) { %w[location contactEmail] }
         let(:non_copyable_attributes) { %w[workflow_state temp_contact_postcode not_even_an_attribute] }
         let(:exclusion_list) { %w[_id email_history] }
 
+
+        context "when the source is a Hash with attributes not in the model" do
+          let(:source_instance) do
+            {
+              "location" => "uk",
+              "contactEmail" => "test@example.com",
+              "deprecated_attribute" => "some value",
+              "another_non_existent_attr" => 123
+            }
+          end
+
+          it "copies only the attributes that exist in the model" do
+            result = run_service
+            expect(result["location"]).to eq("uk")
+            expect(result["contactEmail"]).to eq("test@example.com")
+            expect(result).not_to have_key("deprecated_attribute")
+            expect(result).not_to have_key("another_non_existent_attr")
+          end
+        end
+
+        context "when the source is a BSON::Document with attributes not in the model" do
+          let(:source_instance) do
+            BSON::Document.new(
+              "location" => "uk",
+              "contactEmail" => "test@example.com",
+              "deprecated_attribute" => "some value",
+              "another_non_existent_attr" => 123
+            )
+          end
+
+          it "copies only the attributes that exist in the model" do
+            result = run_service
+            expect(result["location"]).to eq("uk")
+            expect(result["contactEmail"]).to eq("test@example.com")
+            expect(result).not_to have_key("deprecated_attribute")
+            expect(result).not_to have_key("another_non_existent_attr")
+          end
+        end
+
+
         context "when the source is a NewRegistration" do
           let(:source_instance) { build(:new_registration, :has_required_data) }
-
           it_behaves_like "returns the correct attributes"
         end
 
         context "when the source is a RenewingRegistration" do
           let(:source_instance) { build(:renewing_registration, :has_required_data) }
-
           it_behaves_like "returns the correct attributes"
         end
 
         context "when the source is a DeregisteringRegistration" do
-          let(:source_instance) { build(:deregistering_registration) }
-
+          let(:source_instance) { build(:deregistering_registration, location: "uk", contact_email: 'any@email.com') }
           it_behaves_like "returns the correct attributes"
         end
 
         context "when the source is a BSON::Document" do
           let(:source_instance) { BSON::Document.new(build(:new_registration, :has_required_data).attributes) }
-
           it_behaves_like "returns the correct attributes"
+        end
+
+        context "with embedded documents" do
+          let(:source_instance) { build(:new_registration, :has_required_data, :has_addresses, :has_paid_finance_details) }
+
+          it "correctly copies embedded documents" do
+            result = run_service
+            expect(result["addresses"]).to be_an(Array)
+            expect(result["addresses"].first).to include("postcode", "houseNumber")
+            expect(result["finance_details"]).to include("balance")
+          end
         end
       end
 
