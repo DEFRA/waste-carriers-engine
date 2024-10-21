24a809d3494963d4b22ff5c50c4f188d4c111574
Leonid Brujev <brujev@gmail.com>
2024-10-21 17:05:16 +0100

    [RUBY-3433] removing cancacan gem and permission check code associated with user permissions

--- app/services/waste_carriers_engine/base_registration_permission_checks_service.rb
+++ app/services/waste_carriers_engine/base_registration_permission_checks_service.rb
@@ -6,11 +6,10 @@ module WasteCarriersEngine
 
     delegate :registration, to: :transient_registration
 
-    attr_reader :transient_registration, :user, :permission_check_result
+    attr_reader :transient_registration, :permission_check_result
 
-    def run(transient_registration:, user:)
+    def run(transient_registration:)
       @transient_registration = transient_registration
-      @user = user
       @permission_check_result = PermissionChecksResult.new
 
       permission_check_result.pass! if all_checks_pass?
@@ -19,20 +18,6 @@ module WasteCarriersEngine
     end
 
     private
-
-    def can?(action, object)
-      unless user.present?
-        log_transient_registration_details(
-          "Permissions check requested for nil user, action: #{action}",
-          nil,
-          @transient_registration
-        )
-      end
-
-      ability = Ability.new(user)
-
-      ability.can?(action, object)
-    end
 
     def all_checks_pass?
       raise NotImplementedError
