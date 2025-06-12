22a4a160c109da7f25f730118eed465cd19a3d8c
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-06-12 12:26:39 +0100

    Complete registration after payment success webhook

--- app/services/waste_carriers_engine/govpay_payment_webhook_handler.rb
+++ app/services/waste_carriers_engine/govpay_payment_webhook_handler.rb
@@ -6,31 +6,49 @@ module WasteCarriersEngine
       payment_id = webhook_body.dig("resource", "payment_id")
       payment = GovpayFindPaymentService.run(payment_id: payment_id)
 
-      previous_status = payment&.govpay_payment_status
+      previous_status = payment.govpay_payment_status
+      webhook_status = webhook_payment_status(webhook_body, previous_status)
+      if webhook_status == previous_status
+        Rails.logger.debug "No change to payment status #{previous_status} for payment with govpay id \"#{payment_id}\""
+        return
+      end
 
-      result = DefraRubyGovpay::WebhookPaymentService.run(
-        webhook_body,
-        previous_status: previous_status
-      )
-
-      payment_id = result[:id]
-      status = result[:status]
-
-      return if payment.blank?
+      process_payment(payment, webhook_status)
 
       registration = GovpayFindRegistrationService.run(payment: payment)
-      return if registration.blank?
+      process_registration(registration, payment, webhook_status)
 
-      update_payment_status(payment, status)
-
-      complete_renewal_if_ready(registration, status)
-
-      Rails.logger.info "Updated status from #{previous_status} to #{status} for payment #{payment_id}, " \
+      Rails.logger.info "Updated status from #{previous_status} to #{webhook_status} for payment #{payment_id}, " \
                         "registration #{registration.regIdentifier}"
     rescue StandardError => e
       Rails.logger.error "Error processing webhook for payment #{payment_id}: #{e}"
       Airbrake.notify "Error processing webhook for payment #{payment_id}", e
       raise
+    end
+
+    def self.webhook_payment_status(webhook_body, previous_status)
+      DefraRubyGovpay::WebhookPaymentService.run(
+        webhook_body,
+        previous_status: previous_status
+      )[:status]
+    end
+
+    def self.process_payment(payment, webhook_status)
+      if payment.blank?
+        Rails.logger.warn "Payment not found for payment webhook with payment id \"#{payment_id}\""
+        return
+      end
+
+      update_payment_status(payment, webhook_status)
+    end
+
+    def self.process_registration(registration, payment, webhook_status)
+      if registration.blank?
+        Rails.logger.warn "Registration not found for payment with govpay id #{payment.govpay_id}"
+        return
+      end
+
+      complete_registration_or_renewal_if_ready(registration, webhook_status)
     end
 
     def self.update_payment_status(payment, status)
@@ -39,11 +57,18 @@ module WasteCarriersEngine
       (payment.finance_details.registration || payment.finance_details.transient_registration).save!
     end
 
-    def self.complete_renewal_if_ready(registration, status)
-      return unless registration.is_a?(WasteCarriersEngine::RenewingRegistration)
+    def self.complete_registration_or_renewal_if_ready(registration, status)
       return unless status == "success"
 
-      RenewalCompletionService.new(registration).complete_renewal
+      case registration
+      when WasteCarriersEngine::NewRegistration
+        RegistrationCompletionService.new.run(registration)
+      when WasteCarriersEngine::RenewingRegistration
+        RenewalCompletionService.new(registration).complete_renewal
+      else
+        # No need to do anything else for a Registration, this is just for lint purposes
+        Rails.logger.debug "GovpayPaymentWebhookHandler: No completion action for resource type #{registration.class}"
+      end
     end
   end
 end
--- spec/services/waste_carriers_engine/govpay_payment_webhook_handler_spec.rb
+++ spec/services/waste_carriers_engine/govpay_payment_webhook_handler_spec.rb
@@ -50,7 +50,10 @@ module WasteCarriersEngine
           end
 
           context "when the payment is found" do
-            before { registration.finance_details.update_balance }
+            before do
+              registration.finance_details.update_balance
+              registration.save!
+            end
 
             context "when the payment status has not changed" do
               let(:prior_payment_status) { Payment::STATUS_SUCCESS }
@@ -106,6 +109,40 @@ module WasteCarriersEngine
           let(:registration) { create(:registration, :has_required_data) }
 
           it_behaves_like "status is present in the update"
+        end
+
+        context "when the payment belongs to a new registration" do
+          let(:registration) { create(:new_registration, :has_required_data, :has_pending_govpay_status) }
+          let(:registration_completion_service) { instance_double(RegistrationCompletionService) }
+
+          before do
+            allow(RegistrationCompletionService).to receive(:new).and_return(registration_completion_service)
+            allow(registration_completion_service).to receive(:run)
+          end
+
+          it_behaves_like "status is present in the update"
+
+          context "when the status is not success" do
+            let(:prior_payment_status) { "started" }
+
+            before { webhook_body["resource"]["state"]["status"] = "submitted" }
+
+            it "does not call the registration completion service" do
+              run_service
+
+              expect(registration_completion_service).not_to have_received(:run)
+            end
+          end
+
+          context "when the status is success" do
+            before { webhook_body["resource"]["state"]["status"] = "success" }
+
+            it "calls the registration completion service" do
+              run_service
+
+              expect(registration_completion_service).to have_received(:run)
+            end
+          end
         end
 
         context "when the payment belongs to a renewing registration" do
