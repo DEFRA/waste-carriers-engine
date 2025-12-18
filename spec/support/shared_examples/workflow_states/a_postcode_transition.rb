fd2f7a3d25c0e7bd94498a521de7ee8f5eb9e114
Leonid Brujev <brujev@gmail.com>
2025-12-18 18:40:27 +0000

    [RUBY-4039] Update gem dependencies and refactor code to comply with the  latest rubocop coding standards

--- spec/support/shared_examples/workflow_states/a_postcode_transition.rb
+++ spec/support/shared_examples/workflow_states/a_postcode_transition.rb
@@ -2,29 +2,30 @@
 
 RSpec.shared_examples "a postcode transition" do |address_type:, factory:|
   describe "#workflow_state" do
-    current_state = :"#{address_type}_postcode_form"
     subject(:a_subject) do
       create(factory, workflow_state: current_state,
                       tier: defined?(tier) ? tier : WasteCarriersEngine::Registration::UPPER_TIER)
     end
 
+    let(:current_state) { :"#{address_type}_postcode_form" }
+
     context "when subject.skip_to_manual_address? is false" do
-      next_state = :"#{address_type}_address_form"
-      alt_state = :"#{address_type}_address_manual_form"
+      let(:next_state) { :"#{address_type}_address_form" }
+      let(:alt_state) { :"#{address_type}_address_manual_form" }
 
       before { a_subject.temp_os_places_error = nil }
 
-      it "can only transition to either #{next_state} or #{alt_state}" do
+      it "can only transition to the next state or manual address state" do
         permitted_states = Helpers::WorkflowStates.permitted_states(a_subject)
 
         expect(permitted_states).to contain_exactly(next_state, alt_state)
       end
 
-      it "changes to #{next_state} after the 'next' event" do
+      it "changes to the next state after the 'next' event" do
         expect(a_subject).to transition_from(current_state).to(next_state).on_event(:next)
       end
 
-      it "changes to #{alt_state} after the 'skip_to_manual_address' event" do
+      it "changes to the manual address state after the 'skip_to_manual_address' event" do
         expect(a_subject)
           .to transition_from(current_state)
           .to(alt_state)
@@ -33,17 +34,17 @@ RSpec.shared_examples "a postcode transition" do |address_type:, factory:|
     end
 
     context "when subject.skip_to_manual_address? is true" do
-      next_state = :"#{address_type}_address_manual_form"
+      let(:next_state) { :"#{address_type}_address_manual_form" }
 
       before { a_subject.temp_os_places_error = true }
 
-      it "can only transition to either #{next_state}" do
+      it "can only transition to the next state" do
         permitted_states = Helpers::WorkflowStates.permitted_states(a_subject)
 
         expect(permitted_states).to contain_exactly(next_state)
       end
 
-      it "changes to #{next_state} after the 'next' event" do
+      it "changes to the next state after the 'next' event" do
         expect(a_subject).to transition_from(current_state).to(next_state).on_event(:next)
       end
     end
