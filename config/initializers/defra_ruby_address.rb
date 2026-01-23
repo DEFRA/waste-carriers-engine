674e64343facbd28457ee4e61a0dbfc92a797ad5
Leonid Brujev <brujev@gmail.com>
2026-01-23 16:28:20 +0000

    Set default values for address lookup configuration

--- config/initializers/defra_ruby_address.rb
+++ config/initializers/defra_ruby_address.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 DefraRuby::Address.configure do |config|
-  config.host = ENV.fetch("WCRS_OSPLACES_URL")
-  config.key = ENV.fetch("WCRS_OSPLACES_KEY")
+  config.host = ENV.fetch("WCRS_OSPLACES_URL", "https://api.os.uk/search/places/v1")
+  config.key = ENV.fetch("WCRS_OSPLACES_KEY", nil)
 end
