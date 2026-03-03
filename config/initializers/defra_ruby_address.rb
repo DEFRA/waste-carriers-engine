8919ca7707b6cc2b2ccba3c2f67b6d99e3fd4295
Leonid Brujev <brujev@gmail.com>
2026-03-03 19:53:19 +0000

    [RUBY-4163] Replace direct ENV fetches in DefraRuby::Address initializer with Rails.configuration accessors

--- config/initializers/defra_ruby_address.rb
+++ config/initializers/defra_ruby_address.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 DefraRuby::Address.configure do |config|
-  config.host = ENV.fetch("WCRS_OSPLACES_URL", "https://api.os.uk/search/places/v1")
-  config.key = ENV.fetch("WCRS_OSPLACES_KEY", nil)
+  config.host = Rails.configuration.os_places_service_url
+  config.key = Rails.configuration.os_places_api_key
 end
