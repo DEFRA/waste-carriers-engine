bcd1bb2dd116c0b758d23ce10fa26ba7984a36de
Leonid Brujev <brujev@gmail.com>
2026-09-07 15:34:27 +0100

    Upgrade application configuration to load defaults for Rails 8.1

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -16,7 +16,7 @@ require "waste_carriers_engine/detailed_logger"
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults 8.0
+    config.load_defaults 8.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
